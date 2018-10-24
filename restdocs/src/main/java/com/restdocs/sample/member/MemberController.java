7a5a259fb3731f6d93ce20e0b8e6dd55d4a6137f
Yun <cheese10yun@gmail.com>
2018-10-25 04:33:34 +0900

    REST DOCS 작업중

--- restdocs/src/main/java/com/restdocs/sample/member/MemberController.java
+++ restdocs/src/main/java/com/restdocs/sample/member/MemberController.java
@@ -2,6 +2,8 @@ package com.restdocs.sample.member;
 
 import com.restdocs.sample.member.dto.MemberResponseDto;
 import com.restdocs.sample.member.dto.SignUpDto;
+import com.restdocs.sample.model.Address;
+import com.restdocs.sample.model.Email;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,54 @@ public class MemberController {
     @ResponseStatus(HttpStatus.OK)
     public MemberResponseDto signUp(@RequestBody @Valid SignUpDto dto) {
         return new MemberResponseDto(memberService.signUp(dto));
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public MemberResponseDto getUser(@PathVariable Long id) {
+
+        final Address address = Address.builder()
+                .city("city")
+                .street("street")
+                .zipCode("code")
+                .build();
+
+        Email email = Email.builder()
+                .value("test@test.com")
+                .build();
+
+        final Member member = Member.builder()
+                .address(address)
+                .email(email)
+                .build();
+
+        return MemberResponseDto.builder()
+                .member(member)
+                .build();
+
+    }
+
+    @GetMapping
+    public MemberResponseDto getUsers(@RequestParam int page) {
+
+        final Address address = Address.builder()
+                .city("city")
+                .street("street")
+                .zipCode("code")
+                .build();
+
+        Email email = Email.builder()
+                .value("test@test.com")
+                .build();
+
+        final Member member = Member.builder()
+                .address(address)
+                .email(email)
+                .build();
+
+        return MemberResponseDto.builder()
+                .member(member)
+                .build();
+
     }
 
 }
--- restdocs/src/test/java/com/restdocs/sample/member/MemberControllerTest.java
+++ restdocs/src/test/java/com/restdocs/sample/member/MemberControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -22,7 +23,12 @@ import org.springframework.web.context.WebApplicationContext;
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -32,68 +38,129 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class MemberControllerTest {
 
     @Rule
-    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
+//    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
 
     @Autowired
     private WebApplicationContext context;
-
     private ObjectMapper objectMapper = new ObjectMapper();
-
     private MockMvc mockMvc;
-
     private Email email;
     private Address address;
+
+    private RestDocumentationResultHandler documentationHandler;
 
     @Before
     public void setUp() {
         email = buildEmail();
         address = buildAddress();
 
+        this.documentationHandler = document("{method-name}",
+                preprocessRequest(removeHeaders("Authorization")),
+                preprocessResponse(prettyPrint()));
+
 
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
                 .apply(documentationConfiguration(this.restDocumentation))
-                .alwaysDo(document("{class-name}/{method-name}/"))
+//                .alwaysDo(document("{class-name}/{method-name}/"))
+                .alwaysDo(documentationHandler)
                 .build();
     }
 
     @Test
-    public void signUp() throws Exception {
+    public void sign_up() throws Exception {
 
+        // given
         final SignUpDto signUpDto = SignUpDto.builder()
                 .email(email)
                 .address(address)
                 .build();
 
-        final ResultActions resultActions = requestSignUp(signUpDto);
+        // when
+        final ResultActions resultActions = mockMvc.perform(post("/members")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signUpDto)))
+                .andDo(print());
 
+        // then
         resultActions
                 .andExpect(status().isOk())
+//                .andDo(document(
+//                        "sign_up"
+//                ))
                 .andExpect(jsonPath("$.email.value", is(email.getValue())));
 
     }
 
     @Test
-    public void signUpFailed() throws Exception {
+    public void sign_up_fail() throws Exception {
 
         final SignUpDto signUpDto = SignUpDto.builder()
                 .email(Email.builder().value("test").build())
                 .address(address)
                 .build();
 
-        final ResultActions resultActions = requestSignUp(signUpDto);
-
-        resultActions
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.message", is(ErrorCode.INPUT_VALUE_INVALID.message())));
-
-    }
-
-    private ResultActions requestSignUp(SignUpDto signUpDto) throws Exception {
-        return mockMvc.perform(post("/members")
+        final ResultActions resultActions = mockMvc.perform(post("/members")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(signUpDto)))
                 .andDo(print());
+
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", is(ErrorCode.INPUT_VALUE_INVALID.message())))
+                .andDo(document("sign_up_fail"))
+        ;
+
     }
+
+    @Test
+    public void get_member() throws Exception {
+        mockMvc.perform(get("/members/{id}", 1L)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("get_member",
+                        pathParameters(
+                                parameterWithName("id").description("Document's id")
+                        ),
+                        responseFields(
+                                fieldWithPath("email.value").description("The user`s email address"),
+                                fieldWithPath("address.city").description("The user`s email address"),
+                                fieldWithPath("address.street").description("The user`s email address"),
+                                fieldWithPath("address.zipCode").description("The user`s email address")
+                        )
+                ))
+                .andExpect(jsonPath("$.email.value", is("test@test.com")))
+        ;
+
+
+    }
+
+    @Test
+    public void get_members() throws Exception {
+        mockMvc.perform(get("/members?page=1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("get_members",
+
+//                        responseFields(
+//                                fieldWithPath("email.value").description("The user`s email address"),
+//                                fieldWithPath("address.city").description("The user`s email address"),
+//                                fieldWithPath("address.street").description("The user`s email address"),
+//                                fieldWithPath("address.zipCode").description("The user`s email address")
+//                        ),
+                        requestParameters(
+                                parameterWithName("page").description("The page to retrieve")
+                        )
+
+                ))
+                .andExpect(jsonPath("$.email.value", is("test@test.com")))
+        ;
+
+
+    }
+
 
     private Email buildEmail() {
         return Email.builder()
