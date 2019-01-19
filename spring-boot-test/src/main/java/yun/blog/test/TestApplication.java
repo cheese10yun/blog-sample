c40e012afa9c73e02c39049fd6ff37e689333b3a
Yun <cheese10yun@gmail.com>
2019-01-20 00:53:00 +0900

    Test 셈플 코드

---
+++ spring-boot-test/src/main/java/yun/blog/test/TestApplication.java
@@ -0,0 +1,14 @@
+package yun.blog.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+}
+
---
+++ spring-boot-test/src/test/java/yun/blog/test/TestApplicationTests.java
@@ -0,0 +1,17 @@
+package yun.blog.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
