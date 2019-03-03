bb63a9643283ba1babbd036af78f0789cb93562d
Yun <cheese10yun@gmail.com>
2019-03-03 16:11:50 +0900

    mq first commit

---
+++ rabbitmq-sample/src/main/java/yun/blog/rabbitmqsample/RabbitmqSampleApplication.java
@@ -0,0 +1,13 @@
+package yun.blog.rabbitmqsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RabbitmqSampleApplication.class, args);
+  }
+
+}
