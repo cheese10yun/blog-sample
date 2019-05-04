76afaaeab19e94f34558f035448a2e95d70c0f47
Yun <cheese10yun@gmail.com>
2019-05-04 17:23:56 +0900

    proejct init

---
+++ ddd/src/main/java/yun/blog/ddd/DddApplication.java
@@ -0,0 +1,13 @@
+package yun.blog.ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DddApplication.class, args);
+  }
+
+}
