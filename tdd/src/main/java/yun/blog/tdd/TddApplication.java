dee810771eeae926fc63e24edb0b0fdbb7f13883
Yun <cheese10yun@gmail.com>
2019-05-08 01:13:26 +0900

    Tdd proejct first commit

---
+++ tdd/src/main/java/yun/blog/tdd/TddApplication.java
@@ -0,0 +1,13 @@
+package yun.blog.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TddApplication.class, args);
+  }
+
+}
