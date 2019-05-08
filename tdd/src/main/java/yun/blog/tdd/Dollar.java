90a76b8bbc14f9a3d0036e7c05788ed11a22e155
Yun <cheese10yun@gmail.com>
2019-05-09 02:24:29 +0900

    2장 정리

--- tdd/src/main/java/yun/blog/tdd/Dollar.java
+++ tdd/src/main/java/yun/blog/tdd/Dollar.java
@@ -12,8 +12,7 @@ public class Dollar {
     this.amount = amount;
   }
 
-  public void times(int multiplier) {
-
-    amount = amount * multiplier;
+  public Dollar times(int multiplier) {
+    return new Dollar(amount * multiplier);
   }
 }
--- tdd/src/test/java/yun/blog/tdd/MoneyTest.java
+++ tdd/src/test/java/yun/blog/tdd/MoneyTest.java
@@ -6,13 +6,30 @@ import org.junit.Test;
 
 public class MoneyTest {
 
+//  @Test
+//  public void testMultiplication_01() {
+//
+//    Dollar five = new Dollar(5);
+//    five.times(2);
+//
+//    assertThat(five.getAmount()).isEqualTo(10);
+//
+//  }
+
   @Test
-  public void testMultiplication() {
+  public void testMultiplication_02() {
 
     Dollar five = new Dollar(5);
-    five.times(2);
 
-    assertThat(five.getAmount()).isEqualTo(10);
+    Dollar product = five.times(2);
+
+
+    assertThat(product.getAmount()).isEqualTo(10);
+
+
+    product = five.times(3);
+    assertThat(product.getAmount()).isEqualTo(15);
+
 
   }
 }
