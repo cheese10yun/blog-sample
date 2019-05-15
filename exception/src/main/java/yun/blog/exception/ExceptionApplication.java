b02835182b96be86c4e883d5c3ce752e2812a02b
Yun <cheese10yun@gmail.com>
2019-05-15 22:46:17 +0900

    First commit

---
+++ exception/src/main/java/yun/blog/exception/ExceptionApplication.java
@@ -0,0 +1,13 @@
+package yun.blog.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExceptionApplication.class, args);
+  }
+
+}
