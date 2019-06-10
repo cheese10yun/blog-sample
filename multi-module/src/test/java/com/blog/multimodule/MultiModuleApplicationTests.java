8af39d4aa416bc93c74485f64d2b9d9b0e36f7eb
Yun <cheese10yun@gmail.com>
2019-06-10 21:28:59 +0900

    Proejct Init

---
+++ multi-module/src/test/java/com/blog/multimodule/MultiModuleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blog.multimodule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultiModuleApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
