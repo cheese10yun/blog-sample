d6218b8d9010fc2d9e1325cc7fc6a0a8bca2102b
Yun <cheese10yun@gmail.com>
2019-07-14 20:41:37 +0900

    test code first commit

---
+++ test-code/src/main/java/com/example/testcode/TestcodeApplication.java
@@ -0,0 +1,16 @@
+package com.example.testcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcodeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestcodeApplication.class, args);
+  }
+
+
+
+
+}
