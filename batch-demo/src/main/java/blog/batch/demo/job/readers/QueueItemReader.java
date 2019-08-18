bf93e59ab0af31ac3438a851cb0b99d9e7735b6e
Yun <cheese10yun@gmail.com>
2019-08-18 18:26:53 +0900

    코드 수정

--- batch-demo/src/main/java/blog/batch/demo/job/readers/QueueItemReader.java
+++ batch-demo/src/main/java/blog/batch/demo/job/readers/QueueItemReader.java
@@ -10,14 +10,15 @@ import java.util.List;
 import java.util.Queue;
 
 public class QueueItemReader<T> implements ItemReader<T> {
-    private Queue<T> queue;
 
-    public QueueItemReader(List<T> data) {
-        this.queue = new LinkedList<>(data);
-    }
+  private Queue<T> queue;
 
-    @Override
-    public T read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
-        return queue.poll();
-    }
+  public QueueItemReader(List<T> data) {
+    this.queue = new LinkedList<>(data);
+  }
+
+  @Override
+  public T read() {
+    return queue.poll();
+  }
 }
