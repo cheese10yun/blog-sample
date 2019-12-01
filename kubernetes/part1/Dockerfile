208e22714ed185a297f9962f9c703ed9b4c809e7
cheese10yun <cheese10yun@gmail.com>
2019-12-01 23:41:32 +0900

    kubernetes

---
+++ kubernetes/part1/Dockerfile
@@ -0,0 +1,5 @@
+FROM node:slim
+EXPOSE 8000
+COPY hello.js .
+CMD node hello.js
+
---
+++ kubernetes/part1/hello.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var content = function(req, resp) {
+ resp.end("Hello Kubernetes!" + "\n");
+ resp.writeHead(200);
+}
+var w = http.createServer(content);
+w.listen(8000);
