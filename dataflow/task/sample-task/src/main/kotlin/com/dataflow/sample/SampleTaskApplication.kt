fbb2f9ddde6a1f0cc9c1651408820aaf9435621d
cheese10yun <cheese10yun@gmail.com>
2020-01-06 00:09:10 +0900

    add sub module

--- dataflow/task/sample-task/src/main/kotlin/com/dataflow/sample/SampleTaskApplication.kt
+++ dataflow/task/sample-task/src/main/kotlin/com/dataflow/sample/SampleTaskApplication.kt
@@ -1,12 +1,29 @@
 package com.dataflow.sample
 
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.task.configuration.EnableTask
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
-class SampleTaskApplication {
-}
+@EnableTask
+class SampleTaskApplication
 
 fun main(args: Array<String>) {
     runApplication<SampleTaskApplication>(*args)
+}
+
+@Component
+class Sample : ApplicationRunner {
+
+    override fun run(args: ApplicationArguments?) {
+
+        val sourceArgs = args?.sourceArgs
+        val args = sourceArgs?.get(0)
+
+        println("Hello, World! $args" )
+    }
+
 }
