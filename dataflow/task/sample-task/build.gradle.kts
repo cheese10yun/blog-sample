aa0f898c085e1f17223ddb500f57afcf932a06d6
cheese10yun <cheese10yun@gmail.com>
2020-01-06 03:27:04 +0900

    add sub module

--- dataflow/task/sample-task/build.gradle.kts
+++ dataflow/task/sample-task/build.gradle.kts
@@ -11,5 +11,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.cloud:spring-cloud-starter-task")
     runtimeOnly("mysql:mysql-connector-java")
+    implementation("org.mariadb.jdbc:mariadb-java-client")
     testImplementation("org.springframework.batch:spring-batch-test")
 }
--- dataflow/task/sample-task/src/main/kotlin/com/dataflow/sample/BillArgument.kt
+++ dataflow/task/sample-task/src/main/kotlin/com/dataflow/sample/BillArgument.kt
@@ -1,9 +1,10 @@
 package com.dataflow.sample
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.validation.annotation.Validated
 
-
+@ConstructorBinding
 @ConfigurationProperties("argument")
 @Validated
 data class BillArgument(
