583d52c745a3791507a9d98818b31ef914817208
cheese10yun <cheese10yun@gmail.com>
2020-01-13 22:31:32 +0900

    init

---
+++ spring-data-jdbc-sample/src/main/kotlin/com/datajdbc/sample/SampleApplication.kt
@@ -0,0 +1,11 @@
+package com.datajdbc.sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SampleApplication>(*args)
+}
