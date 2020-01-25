5f31b1cf0b82f02b762021d7aaef98ae79954d7d
cheese10yun <cheese10yun@gmail.com>
2020-01-25 23:18:44 +0900

    project init

---
+++ query-dsl/src/main/kotlin/com/example/querydsl/QueryDslApplication.kt
@@ -0,0 +1,11 @@
+package com.example.querydsl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QueryDslApplication
+
+fun main(args: Array<String>) {
+    runApplication<QueryDslApplication>(*args)
+}
