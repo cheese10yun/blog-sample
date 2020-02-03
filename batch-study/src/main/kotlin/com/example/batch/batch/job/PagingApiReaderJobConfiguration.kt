9f745491f98a390a7cfedf9e0e53e6558223cc60
cheese10yun <cheese10yun@gmail.com>
2020-02-03 20:18:31 +0900

    페이징 쿼리

--- batch-study/src/main/kotlin/com/example/batch/batch/job/PagingApiReaderJobConfiguration.kt
+++ batch-study/src/main/kotlin/com/example/batch/batch/job/PagingApiReaderJobConfiguration.kt
@@ -17,7 +17,6 @@ import org.springframework.batch.item.ItemWriter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.math.BigDecimal
-import java.util.function.Consumer
 
 @Configuration
 class PagingApiReaderJobConfiguration(
