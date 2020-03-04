b2a470312b99e07be1a5fa5996183724c3e35620
cheese10yun <cheese10yun@gmail.com>
2020-03-04 22:45:07 +0900

    내용 추가

---
+++ query-dsl/src/main/kotlin/com/example/querydsl/api/Sample.kt
@@ -0,0 +1,23 @@
+package com.example.querydsl.api
+
+import com.example.querydsl.service.ApiService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("sample")
+class Sample(
+    private val apiService: ApiService
+) {
+
+    @GetMapping
+    fun asdasd(): ResponseEntity<String> {
+
+        return apiService.asd()
+
+    }
+
+
+}
--- query-dsl/src/main/kotlin/com/example/querydsl/service/ApiService.kt
+++ query-dsl/src/main/kotlin/com/example/querydsl/service/ApiService.kt
@@ -1,6 +1,7 @@
 package com.example.querydsl.service
 
 import com.example.querydsl.domain.Team
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 
@@ -12,4 +13,11 @@ class ApiService(
     fun getTeam(name: String): List<Team> {
         return restTemplate.getForObject("/teams?name=$name", Array<Team>::class.java)!!.toList()
     }
+
+//    fun asd(): ResponseEntity<String> {
+//        return restTemplate.getForEntity(
+//            "https://sandbox-kard-purchase-api.kakao.com/settlement/purchase?mid=78210046&request_date=2018-02-04&start=0&length=5",
+//            String::class.java
+//        )
+//    }
 }
