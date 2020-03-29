fbd916bdeba58e0e4fd9882845bfd81f4acde51b
cheese10yun <cheese10yun@gmail.com>
2020-03-29 16:35:58 +0900

    project init

---
+++ spring-mock-test/src/main/kotlin/com/example/springmocktest/SpringMockTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springmocktest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMockTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringMockTestApplication>(*args)
+}
---
+++ spring-mock-test/src/test/kotlin/com/example/springmocktest/SpringMockTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springmocktest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMockTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
