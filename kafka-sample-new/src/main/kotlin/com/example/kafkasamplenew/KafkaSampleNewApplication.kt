e3c0ef66c7762e4ac4b7f0fcdde40ea3fb024e74
cheese10yun <cheese10yun@gmail.com>
2020-03-29 20:29:09 +0900

    내용수정

---
+++ kafka-sample-new/src/main/kotlin/com/example/kafkasamplenew/KafkaSampleNewApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kafkasamplenew
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaSampleNewApplication
+
+fun main(args: Array<String>) {
+    runApplication<KafkaSampleNewApplication>(*args)
+}
---
+++ kafka-sample-new/src/test/kotlin/com/example/kafkasamplenew/KafkaSampleNewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kafkasamplenew
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaSampleNewApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
