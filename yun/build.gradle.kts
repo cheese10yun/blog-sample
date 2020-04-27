5958ad3803adb6652cd62bdd789c3b541dfc40de
cheese10yun <cheese10yun@gmail.com>
2020-04-28 01:51:44 +0900

    project init

---
+++ yun/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa") apply false
+    kotlin("kapt")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+allprojects{
+    repositories {
+        jcenter()
+    }
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "kotlin")
+    apply(plugin = "kotlin-spring")
+    apply(plugin = "maven")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "kotlin-kapt")
+
+    dependencies {
+
+        implementation(kotlin("stdlib-jdk8"))
+        implementation(kotlin("reflect"))
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+        implementation("org.springframework.boot:spring-boot-starter")
+
+        testImplementation("org.springframework.boot:spring-boot-starter-test") {
+            exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+        }
+    }
+
+
+    tasks.bootJar {
+        enabled = true
+    }
+
+    tasks.jar {
+        enabled = true
+    }
+
+    tasks.compileKotlin {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "1.8"
+        }
+    }
+
+    tasks.compileTestKotlin {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "1.8"
+        }
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+        maxHeapSize = "1g"
+    }
+}
+
+tasks.bootJar {
+    enabled = false
+}
---
+++ yun/settings.gradle.kts
@@ -0,0 +1,26 @@
+rootProject.name = "yun"
+
+include("api")
+include("domain")
+include("batch")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+
+    val pluginVersions = mapOf(
+        "org.jetbrains.kotlin" to "1.3.71",
+        "org.jetbrains.kotlin.plugin" to "1.3.71",
+        "org.springframework" to "2.2.6.RELEASE",
+        "io.spring" to "1.0.9.RELEASE"
+    )
+
+    resolutionStrategy {
+        eachPlugin {
+            if (pluginVersions.containsKey(requested.id.namespace)) {
+                useVersion(pluginVersions[requested.id.namespace])
+            }
+        }
+    }
+}
