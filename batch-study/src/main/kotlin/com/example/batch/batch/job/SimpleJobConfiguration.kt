beb1b286392a12435f6969dddab7e32f4b8cb729
cheese10yun <cheese10yun@gmail.com>
2020-04-30 02:40:14 +0900

    batch 테스트

--- batch-study/src/main/kotlin/com/example/batch/batch/job/SimpleJobConfiguration.kt
+++ batch-study/src/main/kotlin/com/example/batch/batch/job/SimpleJobConfiguration.kt
@@ -1,5 +1,6 @@
 package com.example.batch.batch.job
 
+import com.example.batch.batch.listener.JobExecutionNotificationListener
 import logger
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
@@ -12,48 +13,49 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SimpleJobConfiguration(
-        private val jobBuilderFactory: JobBuilderFactory,
-        private val stepBuilderFactory: StepBuilderFactory
+    private val jobBuilderFactory: JobBuilderFactory,
+    private val stepBuilderFactory: StepBuilderFactory
 ) {
     private val log by logger()
 
     @Bean
     fun simpleJob(): Job {
         return jobBuilderFactory.get("simpleJob")
-                .incrementer(RunIdIncrementer())
-                .start(simpleStep1())
-                .next(simpleStep2())
-                .next(simpleStep3())
-                .build()
+            .incrementer(RunIdIncrementer())
+            .listener(JobExecutionNotificationListener())
+            .start(simpleStep1())
+            .next(simpleStep2())
+            .next(simpleStep3())
+            .build()
     }
 
     @Bean
     fun simpleStep1(): Step {
         return stepBuilderFactory.get("simpleStep1")
-                .tasklet { contribution, chunkContext ->
-                    log.info("This is Step 1")
-                    RepeatStatus.FINISHED
-                }
-                .build()
+            .tasklet { contribution, chunkContext ->
+                log.info("This is Step 1")
+                RepeatStatus.FINISHED
+            }
+            .build()
     }
 
     @Bean
     fun simpleStep2(): Step {
         return stepBuilderFactory.get("simpleStep1")
-                .tasklet { contribution, chunkContext ->
-                    log.info("This is Step 2")
-                    RepeatStatus.FINISHED
-                }
-                .build()
+            .tasklet { contribution, chunkContext ->
+                log.info("This is Step 2")
+                RepeatStatus.FINISHED
+            }
+            .build()
     }
 
     @Bean
     fun simpleStep3(): Step {
         return stepBuilderFactory.get("simpleStep1")
-                .tasklet { _, _ ->
-                    log.info("This is Step 3")
-                    RepeatStatus.FINISHED
-                }
-                .build()
+            .tasklet { _, _ ->
+                log.info("This is Step 3")
+                RepeatStatus.FINISHED
+            }
+            .build()
     }
 }
---
+++ yun/README.md
@@ -0,0 +1,13 @@
+# ????
+
+## Repository 비공개
+
+## FindService 
+비공개 하고 싶은 이유가 그거다... 
+
+## api -> service 
+* embedded 각자 두기
+* projections 각각 두기
+* request, 재활용하지 않기
+
+
