2e2121f2c63a3914f8e9c9050ad7bc18f7f3f3a2
cheese10yun <cheese10yun@gmail.com>
2020-05-02 22:54:33 +0900

    commit

--- yun/api/build.gradle.kts
+++ yun/api/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     api(project(":domain"))
+    api(project(":service:service-order"))
 
     runtimeOnly("com.h2database:h2")
 }
--- yun/api/src/main/kotlin/com/cheese/yun/api/OrderApi.kt
+++ yun/api/src/main/kotlin/com/cheese/yun/api/OrderApi.kt
@@ -3,6 +3,7 @@ package com.cheese.yun.api
 import com.cheese.yun.domain.model.Address
 import com.cheese.yun.domain.order.Order
 import com.cheese.yun.domain.order.OrderRepository
+import com.cheese.yun.service.order.OrderService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,13 +13,21 @@ import java.math.BigDecimal
 @RestController
 @RequestMapping("api/orders")
 class OrderApi(
-    private val orderRepository: OrderRepository
+    private val orderRepository: OrderRepository,
+    private val orderService: OrderService
 ) {
 
     @PostMapping
     fun order(@RequestBody dto: OrderRequest) {
         orderRepository.save(Order(dto.address, dto.price))
     }
+
+    @PostMapping("/2")
+    fun order2(@RequestBody dto: OrderRequest){
+        orderRepository.save(Order(dto.address, dto.price))
+    }
+
+
 }
 
 data class OrderRequest(
