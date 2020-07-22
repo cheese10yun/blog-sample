6bee3c5709aa75deb1a2e9ec37f159343ee7a809
cheese10yun <cheese10yun@gmail.com>
2020-07-22 21:24:05 +0900

    webflux api

--- web-flux/build.gradle.kts
+++ web-flux/build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
