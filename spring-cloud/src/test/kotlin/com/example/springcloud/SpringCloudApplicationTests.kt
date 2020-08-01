a49c6d523434d262b8477ace358f946516cd98b9
cheese10yun <cheese10yun@gmail.com>
2020-08-01 23:59:56 +0900

    project init

---
+++ spring-cloud/src/test/kotlin/com/example/springcloud/SpringCloudApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springcloud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCloudApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
