8b61f2ae8907b41271d43fc15ce8d27bc026b6da
cheese10yun <cheese10yun@gmail.com>
2020-08-02 11:39:51 +0900

    proejct init

---
+++ msa-koltin/organization-service/src/main/kotlin/com/example/organizationservice/OrganizationServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.organizationservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrganizationServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrganizationServiceApplication>(*args)
+}
---
+++ msa-koltin/organization-service/src/test/kotlin/com/example/organizationservice/OrganizationServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.organizationservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrganizationServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
