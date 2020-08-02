264ca51b4ba6f9d2b9d73ee3bc97bc9b8f32a731
cheese10yun <cheese10yun@gmail.com>
2020-08-02 11:59:47 +0900

    proejct init

--- msa-koltin/organization-service/build.gradle.kts
+++ msa-koltin/organization-service/build.gradle.kts
@@ -21,6 +21,7 @@ extra["springCloudVersion"] = "Hoxton.SR6"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
