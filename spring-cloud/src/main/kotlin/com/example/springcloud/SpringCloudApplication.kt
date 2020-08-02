a0825dd1591a8df063420a8e8fafb41295977e34
cheese10yun <cheese10yun@gmail.com>
2020-08-02 12:18:48 +0900

    init project

---
+++ spring-cloud/src/main/kotlin/com/example/springcloud/SpringCloudApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springcloud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCloudApplication>(*args)
+}
