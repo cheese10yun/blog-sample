e91bb0edac7c9b06434c0aaad991631357ef2d75
cheese10yun <cheese10yun@gmail.com>
2020-08-02 14:52:33 +0900

    init project

--- spring-cloud/settings.gradle.kts
+++ spring-cloud/settings.gradle.kts
@@ -1,1 +1,25 @@
 rootProject.name = "spring-cloud"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven(url = "http://repo.kakaopay.com/repository/kakaopay-public") {
+            this.isAllowInsecureProtocol = true
+        }
+    }
+
+    val pluginVersions = mapOf(
+        "org.jetbrains.kotlin" to "1.3.72",
+        "org.jetbrains.kotlin.plugin" to "1.3.72",
+        "org.springframework" to "2.3.2.RELEASE",
+        "io.spring" to "1.0.9.RELEASE"
+    )
+
+    resolutionStrategy {
+        eachPlugin {
+            if (pluginVersions.containsKey(requested.id.namespace)) {
+                useVersion(pluginVersions[requested.id.namespace])
+            }
+        }
+    }
+}
