2ee721678da17013f037864a571ceddeced39b11
cheese10yun <cheese10yun@gmail.com>
2020-08-03 01:32:29 +0900

    init project

--- spring-cloud/zuul-server/build.gradle.kts
+++ spring-cloud/zuul-server/build.gradle.kts
@@ -2,5 +2,5 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-zuul")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-hystrix")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-    implementation("org.springframework.cloud:spring-cloud-starter-config")
+//    implementation("org.springframework.cloud:spring-cloud-starter-config")
 }
