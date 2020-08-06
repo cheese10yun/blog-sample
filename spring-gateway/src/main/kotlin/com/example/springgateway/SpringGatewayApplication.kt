98004779a5d155fba96d6c425651f0dacecdbf5a
cheese10yun <cheese10yun@gmail.com>
2020-08-06 14:43:59 +0900

    commit

---
+++ spring-gateway/src/main/kotlin/com/example/springgateway/SpringGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springgateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringGatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringGatewayApplication>(*args)
+}
