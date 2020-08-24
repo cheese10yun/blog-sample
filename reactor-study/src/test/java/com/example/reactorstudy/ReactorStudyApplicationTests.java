21f2f292cd583d0a8ab7e96b021224f73214c411
cheese10yun <cheese10yun@gmail.com>
2020-08-25 00:09:21 +0900

    init

--- reactor-study/src/test/java/com/example/reactorstudy/ReactorStudyApplicationTests.java
+++ reactor-study/src/test/java/com/example/reactorstudy/ReactorStudyApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.reactorstudy;
 
+import java.text.MessageFormat;
+import java.time.Duration;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
 
-@SpringBootTest
+
 class ReactorStudyApplicationTests {
 
     @Test
-    void contextLoads() {
+    void name() {
+        Flux.range(1, 100)
+            .subscribe(
+                data -> System.out.println(MessageFormat.format("onNext:'{'{0}'}'", data)),
+                err -> System.out.println("error ignore"),
+                () -> System.out.println("onComplete"),
+                subscription -> {
+                    subscription.request(4);
+                    subscription.cancel();
+                }
+            )
+        ;
     }
 
+    @Test
+    void test01() throws InterruptedException {
+        final Disposable disposable = Flux.interval(Duration.ofMillis(50))
+            .subscribe(
+                data -> System.out.println(MessageFormat.format("onNext:'{'{0}'}'", data))
+                );
+
+        Thread.sleep(200);
+        disposable.dispose();
+
+
+    }
 }
