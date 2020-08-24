723849ba87e47f6258bd1e7221550384a24d3c69
cheese10yun <cheese10yun@gmail.com>
2020-08-24 22:43:16 +0900

    init

---
+++ reactor-study/src/main/java/com/example/reactorstudy/ReactorStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactorstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactorStudyApplication.class, args);
+    }
+
+}
---
+++ reactor-study/src/test/java/com/example/reactorstudy/ReactorStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactorstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
