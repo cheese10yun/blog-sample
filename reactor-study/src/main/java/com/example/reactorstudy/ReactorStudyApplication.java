236e2815498d18156b36d22266a30c83ec254dfe
cheese10yun <cheese10yun@gmail.com>
2020-08-26 09:11:54 +0900

    init

--- reactor-study/src/main/java/com/example/reactorstudy/ReactorStudyApplication.java
+++ reactor-study/src/main/java/com/example/reactorstudy/ReactorStudyApplication.java
@@ -2,6 +2,9 @@ package com.example.reactorstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @SpringBootApplication
 public class ReactorStudyApplication {
@@ -9,5 +12,8 @@ public class ReactorStudyApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReactorStudyApplication.class, args);
     }
+
+//    @Bean
+//    public RouterFunction<ServerResponse> routes()
 
 }
