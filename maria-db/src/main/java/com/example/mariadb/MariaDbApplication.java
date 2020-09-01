a0aa968ae8215be78360f156ef8381e2da4189b0
cheese10yun <cheese10yun@gmail.com>
2020-09-01 23:56:10 +0900

    init

---
+++ maria-db/src/main/java/com/example/mariadb/MariaDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.mariadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariaDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MariaDbApplication.class, args);
+    }
+
+}
---
+++ maria-db/src/test/java/com/example/mariadb/MariaDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mariadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariaDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
