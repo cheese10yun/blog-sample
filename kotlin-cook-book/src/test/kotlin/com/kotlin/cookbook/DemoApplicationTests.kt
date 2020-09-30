0b9e61ec0abcbe2cbe4d39be94984b51b3b75bfb
cheese10yun <cheese10yun@gmail.com>
2020-09-30 10:02:00 +0900

    project init

---
+++ kotlin-cook-book/src/test/kotlin/com/kotlin/cookbook/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.cookbook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
