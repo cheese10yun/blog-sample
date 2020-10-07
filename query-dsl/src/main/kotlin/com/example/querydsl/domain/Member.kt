ff4c91a05f8089f89ac4bc72ab0616c8f9b3be98
cheese10yun <cheese10yun@gmail.com>
2020-10-08 00:32:33 +0900

    의존성 추가

--- query-dsl/src/main/kotlin/com/example/querydsl/domain/Member.kt
+++ query-dsl/src/main/kotlin/com/example/querydsl/domain/Member.kt
@@ -1,10 +1,19 @@
 package com.example.querydsl.domain
 
-import javax.persistence.*
+import org.hibernate.annotations.Where
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.FetchType
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
 
 
 @Entity
 @Table(name = "member")
+@Where(clause = "status = NORMAL")
 data class Member(
     @Column(name = "username", nullable = false)
     var username: String,
@@ -15,4 +24,14 @@ data class Member(
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "team_id", nullable = false)
     var team: Team
-) : EntityAuditing()
+) : EntityAuditing() {
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    var status: MemberStatus = MemberStatus.NORMAL
+}
+
+enum class MemberStatus {
+    NORMAL,
+    BAN
+}
