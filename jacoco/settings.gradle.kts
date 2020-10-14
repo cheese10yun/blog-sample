e7703f42e0129a307cdcd18d4ae847c20252138e
cheese10yun <cheese10yun@gmail.com>
2020-10-15 00:24:09 +0900

    project init

---
+++ jacoco/settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "jacoco"
+
+
+pluginManagement {
+    val pluginVersions = mapOf(
+        "org.jetbrains.kotlin" to "1.3.72",
+        "org.jetbrains.kotlin.plugin" to "1.3.72",
+        "org.springframework" to "2.3.4.RELEASE",
+        "io.spring" to "1.0.10.RELEASE"
+    )
+
+    resolutionStrategy {
+        eachPlugin {
+            if (pluginVersions.containsKey(requested.id.namespace)) {
+                useVersion(pluginVersions[requested.id.namespace])
+            }
+        }
+    }
+}
