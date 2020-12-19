7610cbb6fe014b9cb5b72ba5f00f500b611e0e34
cheese10yun <cheese10yun@gmail.com>
2020-12-19 22:36:08 +0900

    add module

--- batch-study/build.gradle.kts
+++ batch-study/build.gradle.kts
@@ -8,18 +8,19 @@ plugins {
     kotlin("plugin.jpa") version "1.4.21"
 }
 
-allOpen {
-    annotation("javax.persistence.Entity")
-    annotation("javax.persistence.MappedSuperclass")
-    annotation("javax.persistence.Embeddable")
-}
-
 group = "com.batch"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
+    jcenter()
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
 }
 
 dependencies {
@@ -27,8 +28,15 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("org.jetbrains.exposed:exposed-core:${properties["exposed_version"]}")
+    implementation("org.jetbrains.exposed:exposed-dao:${properties["exposed_version"]}")
+    implementation("org.jetbrains.exposed:exposed-jdbc:${properties["exposed_version"]}")
+    implementation("org.jetbrains.exposed:exposed-java-time:${properties["exposed_version"]}")
+
     runtimeOnly("com.h2database:h2")
     runtimeOnly("mysql:mysql-connector-java")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.batch:spring-batch-test")
 }
