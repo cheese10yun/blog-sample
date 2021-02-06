782bfd216042bd80da2e9b190c99ca663e344666
cheese10yun <cheese10yun@gmail.com>
2021-02-06 23:54:26 +0900

    commit

--- batch-study/build.gradle.kts
+++ batch-study/build.gradle.kts
@@ -37,6 +37,8 @@ subprojects {
 
 
     dependencies {
+
+        implementation("org.springframework.boot:spring-boot-starter")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
