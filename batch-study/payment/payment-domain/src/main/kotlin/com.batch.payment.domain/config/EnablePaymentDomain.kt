51be040705ee44c8853672408935a31a31006a91
cheese10yun <cheese10yun@gmail.com>
2021-02-08 02:52:57 +0900

    commit

--- batch-study/payment/payment-domain/src/main/kotlin/com.batch.payment.domain/config/EnablePaymentDomain.kt
+++ batch-study/payment/payment-domain/src/main/kotlin/com.batch.payment.domain/config/EnablePaymentDomain.kt
@@ -1,9 +1,20 @@
 package com.batch.payment.domain.config
 
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+const val basePackages = "com.batch.payment.domain"
 
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FILE)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
-@ComponentScan(basePackages = ["com.batch.payment.domain"])
+@ComponentScan(basePackages = [basePackages])
+@EnableJpaRepositories(
+    basePackages = [basePackages],
+    entityManagerFactoryRef = "entityManagerFactory",
+    transactionManagerRef = "transactionManager",
+
+    )
+@EntityScan(basePackages = [basePackages])
 annotation class EnablePaymentDomain
