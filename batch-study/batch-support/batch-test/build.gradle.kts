ede451151407b66ed156801b73fddf385309fd6d
cheese10yun <cheese10yun@gmail.com>
2021-02-20 11:51:41 +0900

    Add exposed batch insert

--- batch-study/batch-support/batch-test/build.gradle.kts
+++ batch-study/batch-support/batch-test/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-test")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.batch:spring-batch-test")
+    api("org.springframework.boot:spring-boot-starter-test")
+    api("org.springframework.boot:spring-boot-starter-data-jpa")
+    api("org.springframework.batch:spring-batch-test")
     api("com.querydsl:querydsl-jpa")
 }
