da1e947f70b92215954b641f058c91f8871633a0
cheese10yun <cheese10yun@gmail.com>
2021-05-02 02:12:14 +0900

    init

---
+++ test-strategy/src/main/kotlin/com/example/teststrategy/TestStrategyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.teststrategy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestStrategyApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestStrategyApplication>(*args)
+}
---
+++ test-strategy/src/test/kotlin/com/example/teststrategy/TestStrategyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.teststrategy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestStrategyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
