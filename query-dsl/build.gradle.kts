b1ee0515c1c5528f057d9cc47351056a56787af3
cheese10yun <cheese10yun@gmail.com>
2021-05-23 12:27:31 +0900

    버전 변경

--- query-dsl/build.gradle.kts
+++ query-dsl/build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.2.4.RELEASE"
-    id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    kotlin("jvm") version "1.3.61"
-    kotlin("plugin.spring") version "1.3.61"
-    kotlin("plugin.jpa") version "1.3.61"
-    kotlin("kapt") version "1.3.61"
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
+    kotlin("kapt")
 }
 
 group = "com.example"
--- query-dsl/settings.gradle.kts
+++ query-dsl/settings.gradle.kts
@@ -1,1 +1,23 @@
 rootProject.name = "query-dsl"
+
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+
+    val pluginVersions = mapOf(
+        "org.jetbrains.kotlin" to "1.4.21",
+        "org.jetbrains.kotlin.plugin" to "1.4.21",
+        "org.springframework" to "2.4.1",
+        "io.spring" to "1.0.10.RELEASE"
+    )
+
+    resolutionStrategy {
+        eachPlugin {
+            if (pluginVersions.containsKey(requested.id.namespace)) {
+                useVersion(pluginVersions[requested.id.namespace])
+            }
+        }
+    }
+}
