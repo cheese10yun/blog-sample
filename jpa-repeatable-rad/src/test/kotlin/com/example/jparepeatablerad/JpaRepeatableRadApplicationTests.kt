3eae90dab1e29ccdf7c26a19f7d71634f4d6b2ee
cheese10yun <cheese10yun@gmail.com>
2021-06-19 13:59:53 +0900

    init

---
+++ jpa-repeatable-rad/src/test/kotlin/com/example/jparepeatablerad/JpaRepeatableRadApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jparepeatablerad
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaRepeatableRadApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
