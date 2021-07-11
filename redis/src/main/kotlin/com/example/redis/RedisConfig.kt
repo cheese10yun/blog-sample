a5b25f85c07d09a940f4f6d2e615ae153fca6adc
cheese10yun <cheese10yun@gmail.com>
2021-07-12 00:13:50 +0900

    redis init

--- redis/src/main/kotlin/com/example/redis/RedisConfig.kt
+++ redis/src/main/kotlin/com/example/redis/RedisConfig.kt
@@ -1,37 +1,36 @@
-//package com.example.redis
-//
-//import org.springframework.context.annotation.Bean
-//import org.springframework.context.annotation.Configuration
-//import org.springframework.context.annotation.Primary
-//import org.springframework.data.redis.connection.RedisPassword
-//import org.springframework.data.redis.connection.RedisStandaloneConfiguration
-//import org.springframework.data.redis.connection.lettuce.LettuceConnection
-//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
-//import org.springframework.data.redis.core.RedisTemplate
-//
-//@Configuration
-//class RedisConfig {
-//
-//
-//    @Bean
-//    @Primary
-//    fun redisTemplate(redisConnectionFactory: LettuceConnectionFactory): RedisTemplate<ByteArray, ByteArray> =
-//        RedisTemplate<ByteArray, ByteArray>().apply {
-//            setConnectionFactory(redisConnectionFactory)
-//        }
-//
-//
-//    @Bean
-//    @Primary
-//    fun redisConnectionFactory(): LettuceConnectionFactory {
-//        return LettuceConnectionFactory(
-//            RedisStandaloneConfiguration("localhost", 6379)
-//                .also { it.password = RedisPassword.of("root") }
-//        ).also {
-//            it.setPipeliningFlushPolicy(
-//                LettuceConnection.PipeliningFlushPolicy.buffered(1)
-//            )
-//        }
-//    }
-//
-//}
+package com.example.redis
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnection
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+
+@Configuration
+class RedisConfig {
+
+
+    @Bean
+    @Primary
+    fun redisTemplate(redisConnectionFactory: LettuceConnectionFactory): RedisTemplate<ByteArray, ByteArray> =
+        RedisTemplate<ByteArray, ByteArray>().apply {
+            setConnectionFactory(redisConnectionFactory)
+        }
+
+
+    @Bean
+    @Primary
+    fun redisConnectionFactory(): LettuceConnectionFactory =
+        LettuceConnectionFactory(
+            RedisStandaloneConfiguration("localhost", 6379)
+                .apply { password = RedisPassword.of("root") }
+        ).apply {
+            setPipeliningFlushPolicy(
+                LettuceConnection.PipeliningFlushPolicy.buffered(1000)
+            )
+        }
+
+}
--- redis/src/test/kotlin/com/example/redis/RedisApplicationTests.kt
+++ redis/src/test/kotlin/com/example/redis/RedisApplicationTests.kt
@@ -12,44 +12,58 @@ import org.springframework.util.StopWatch
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 class RedisApplicationTests(
-//    val memberRepository: MemberRepository,
+    val memberRepository: MemberRepository,
     val redisTemplate: RedisTemplate<ByteArray, ByteArray>
 ) {
 
+    private val memberCount = 5000
+
     @Test
     fun `members`() {
-        val members = (1..10_000).map {
+        val members = (1..memberCount).map {
             Member(it.toLong())
         }
         val stopWatch = StopWatch()
         stopWatch.start()
-//        memberRepository.saveAll(members)
+        memberRepository.saveAll(members)
         stopWatch.stop()
+
+
+        println(stopWatch.lastTaskTimeMillis)
+        memberRepository.deleteAll()
+
+    }
+
+    @Test
+    fun `pipe line`() {
+        val members = (1..memberCount).map {
+            Member(it.toLong())
+        }
+
+        val stopWatch = StopWatch()
+        stopWatch.start()
 
         redisTemplate.execute { rc ->
             rc.openPipeline()
             members.forEach { member ->
-
                 member.toString().toByteArray() to member.toString().toByteArray()
                 val mutableMap = mutableMapOf<ByteArray, ByteArray>()
-
                 val toByteArray = "member-${member.id}".toByteArray()
-
                 mutableMap[toByteArray] = member.toString().toByteArray()
-
-
                 rc.hMSet(toByteArray, mutableMap)
             }
             rc.closePipeline()
         }!!
 
-
+        stopWatch.stop()
         println(stopWatch.lastTaskTimeMillis)
-        // 1,000 = 66811
-        // 1 = 62877
-        // 46757
+        memberRepository.deleteAll()
 
-//        memberRepository.deleteAll()
 
+        redisTemplate.execute {
+            it.flushAll()
+        }
     }
 }
+
+// 37352
