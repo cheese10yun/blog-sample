d4e43c5d5ce7155a6bdbb6531f13793732e839ab
cheese10yun <cheese10yun@gmail.com>
2021-07-13 19:36:34 +0900

    commit
    Former-commit-id: e4b90c4137493dca0ce12aee39a3c74aac4ac6ab

--- elk-sample/api/src/main/kotlin/com/example/api/SampleApi.kt
+++ elk-sample/api/src/main/kotlin/com/example/api/SampleApi.kt
@@ -1,12 +1,18 @@
 package com.example.api
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import javax.validation.Valid
+import javax.validation.constraints.Email
 import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.web.bind.annotation.*
-import javax.validation.Valid
-import javax.validation.constraints.Email
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/sample")
@@ -16,6 +22,37 @@ class SampleApi {
 
     @PostMapping
     fun sample(@RequestBody @Valid sample: SampleRequest) = sample
+}
+
+@Service
+class MemberService(
+    val memberRepository: MemberRepository,
+    private val objectMapper: ObjectMapper
+) {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @Transactional
+    fun memberxxxx(pageable: Pageable): Page<Member> {
+
+        aaaa()
+        bbbb()
+        val members = memberRepository.findAll(pageable)
+        logger.info(objectMapper.writeValueAsString(members))
+
+        return members
+
+    }
+
+    fun aaaa(){
+        memberRepository.findById(1L)
+        memberRepository.findById(2L)
+        memberRepository.findById(3L)
+
+    }
+
+    fun bbbb(){
+        Thread.sleep(100)
+    }
 }
 
 
@@ -34,20 +71,17 @@ data class AAA(
 @RequestMapping("/members")
 class MemberApi(
     private val memberRepository: MemberRepository,
-    private val objectMapper: ObjectMapper
+    private val memeberService: MemberService
 ) {
-    private val logger = LoggerFactory.getLogger(javaClass)
+
 
     @GetMapping
-    fun getMembers(pageable: Pageable): Page<Member> {
-        val members = memberRepository.findAll(pageable)
-        logger.info(objectMapper.writeValueAsString(members))
-        return members
-    }
+    fun getMembers(pageable: Pageable) = memeberService.memberxxxx(pageable)
+
 
     @GetMapping("/test")
     fun asd() {
-        if(true){
+        if (true) {
             throw IllegalStateException("server error test error")
         }
     }
