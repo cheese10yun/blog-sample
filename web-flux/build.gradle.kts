3d3bb4f81499949812acd469d0c6f1a54bc36d22
cheese10yun <cheese10yun@gmail.com>
2021-08-30 23:46:08 +0900

    Add Local test

--- web-flux/build.gradle.kts
+++ web-flux/build.gradle.kts
@@ -3,12 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.5.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id ("com.gorylenko.gradle-git-properties") version "2.3.1"
     kotlin("jvm") version "1.5.21"
     kotlin("plugin.spring") version "1.5.21"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
+
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
@@ -16,8 +18,14 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+//    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+//    implementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
+//    implementation("org.mongodb:mongodb-driver-sync")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
--- web-flux/src/main/kotlin/com/example/webflux/ServiceController.kt
+++ web-flux/src/main/kotlin/com/example/webflux/ServiceController.kt
@@ -3,10 +3,12 @@ package com.example.webflux
 import java.time.Duration
 import java.util.Random
 import org.springframework.http.MediaType
+import org.springframework.stereotype.Controller
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import reactor.core.publisher.SynchronousSink
 
 @RestController
@@ -16,6 +18,13 @@ class ServiceController(
 
     @GetMapping(value = ["/server"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
     fun serverDishes() = kitchenService.getDishes()
+}
+
+@Controller
+class HomeController {
+
+    @GetMapping
+    fun home() = Mono.just("home")
 }
 
 @Service
@@ -28,19 +37,36 @@ class KitchenService {
         Dish("Sweet & sour beef")
     )
 
-
+    /**
+     * 요리 스트림 생성
+     */
     fun getDishes() =
         Flux.generate { sink: SynchronousSink<Dish> ->
             sink.next(randomDish())
         }
             .delayElements(Duration.ofMillis(250))
 
+    /**
+     * 요리 무작위 선택
+     */
     private fun randomDish() =
         menu[picker.nextInt(menu.size)]
-
 
 }
 
 data class Dish(
-    val name: String
-)
+    val description: String
+) {
+    var delivered = false
+        private set
+
+    companion object {
+
+        fun deliver(dish: Dish) {
+            val deliverDish = Dish(dish.description)
+            deliverDish.also {
+                it.delivered = true
+            }
+        }
+    }
+}
