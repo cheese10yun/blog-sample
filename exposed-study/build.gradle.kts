d7dd6af1155daaad9d1127596945fa031245b21d
cheese10yun <cheese10yun@gmail.com>
2021-10-23 16:49:50 +0900

    gradle 버전 변경 및 spring boot version 변경

--- exposed-study/build.gradle.kts
+++ exposed-study/build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.1"
+    id("org.springframework.boot") version "2.5.6"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     kotlin("jvm") version "1.4.21"
     kotlin("plugin.spring") version "1.4.21"
-//    kotlin("plugin.jpa") version "1.4.21"
+    kotlin("plugin.jpa") version "1.4.21"
 }
 
 group = "com.example"
@@ -19,7 +19,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
