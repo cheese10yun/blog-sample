2ea64b9b20b3b6f7844c610c9e0c25959c30f4f4
cheese10yun <cheese10yun@gmail.com>
2021-12-11 15:06:59 +0900

    new macbook test commit

---
+++ kotlin-jpa/docs/intellij-string-manipulation.md
@@ -0,0 +1,25 @@
+# String Manipulation
+
+[String Manipulation Plugin](https://plugins.jetbrains.com/plugin/2162-string-manipulation/)에 사용법에 대한 포스팅입니다.
+
+## Increment 기능
+
+![](https://raw.githubusercontent.com/cheese10yun/IntelliJ/master/assets/string-manipulation-1.gif)
+
+특정 값에 대해서 자동으로 증가시켜 중복되지 않는 값으로 설정할 수 있습니다.
+
+![](string-maniplation-2.png)
+
+위 이미지와 같은 FK 참조하는 값에 대한 SQL을 작성하는 경우 매우 효율 적입니다. 이전에 [Sql을 통해서 테스트 코드를 쉽게 작성하자](https://cheese10yun.github.io/sql-test/) 포스팅도 참고하시면 좋을 거 같습니다.
+
+## Switch
+
+![](string-manipulation-3.gif)
+
+Switch는 다양한 문자열 포맷으로 쉽게 변경이 가능합니다.
+
+![](string-manipulation-4.png)
+
+Switch Case 항목을 보시면 매우 다양한 항목으로 변경이 가능합니다.
+
+저 같은 경우는 snake_case json을 기반으로 DTO 클래스를 만들 때 유용하게 사용하고 있습니다.
---
+++ kotlin-jpa/src/main/resources/test.sql
@@ -0,0 +1,35 @@
+insert into member('id', 'name')
+values (1, 'name-1'),
+       (2, 'name-1'),
+       (3, 'name-1'),
+       (4, 'name-1'),
+       (5, 'name-1'),
+       (6, 'name-1'),
+       (7, 'name-1'),
+       (8, 'name-1'),
+       (9, 'name-1'),
+       (10, 'name-1'),
+       (11, 'name-1'),
+       (12, 'name-1'),
+       (13, 'name-1'),
+       (14, 'name-1'),
+       (15, 'name-1')
+;
+
+insert into order ('member_id', 'order_number')
+values (1, 'order_number-1'),
+       (2, 'order_number-1'),
+       (3, 'order_number-1'),
+       (4, 'order_number-1'),
+       (5, 'order_number-1'),
+       (6, 'order_number-1'),
+       (7, 'order_number-1'),
+       (8, 'order_number-1'),
+       (9, 'order_number-1'),
+       (10, 'order_number-1'),
+       (11, 'order_number-1'),
+       (12, 'order_number-1'),
+       (13, 'order_number-1'),
+       (14, 'order_number-1'),
+       (15, 'order_number-1')
+;
