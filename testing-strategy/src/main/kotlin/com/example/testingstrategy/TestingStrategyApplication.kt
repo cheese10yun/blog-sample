e7549d431947b122b5bdc64b64d7e0b7634df0cf
cheese10yun <cheese10yun@gmail.com>
2021-12-11 20:56:58 +0900

    commit

---
+++ testing-strategy/src/main/kotlin/com/example/testingstrategy/TestingStrategyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.testingstrategy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestingStrategyApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestingStrategyApplication>(*args)
+}
