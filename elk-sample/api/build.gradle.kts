df824bf487f4eb2cfa63d81d36f35503e29fa61a
cheese10yun <cheese10yun@gmail.com>
2022-02-19 13:21:46 +0900

    Modify test

--- elk-sample/api/build.gradle.kts
+++ elk-sample/api/build.gradle.kts
@@ -29,10 +29,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("net.logstash.logback:logstash-logback-encoder:6.3")
-    implementation("mysql:mysql-connector-java")
+//    implementation("mysql:mysql-connector-java")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-//	runtimeOnly("com.h2database:h2")
+	runtimeOnly("com.h2database:h2")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
--- elk-sample/api/src/main/kotlin/com/example/api/ApiApplication.kt
+++ elk-sample/api/src/main/kotlin/com/example/api/ApiApplication.kt
@@ -16,17 +16,17 @@ fun main(args: Array<String>) {
 
 @Component
 class AppRunner(
-        private val memberRepository: MemberRepository
+    private val memberRepository: MemberRepository
 ) : ApplicationRunner {
 
     override fun run(args: ApplicationArguments?) {
 
         val members = listOf(
-                Member("name", "asd@asd.com"),
-                Member("name1", "asd1@asd.com"),
-                Member("name2", "asd2@asd.com"),
-                Member("name3", "asd3@asd.com"),
-                Member("name4", "asd4@asd.com")
+            Member("name", "asd@asd.com"),
+            Member("name1", "asd1@asd.com"),
+            Member("name2", "asd2@asd.com"),
+            Member("name3", "asd3@asd.com"),
+            Member("name4", "asd4@asd.com")
         )
         memberRepository.saveAll(members)
     }
