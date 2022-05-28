674a2f31b6f6d675b75dff5f3b964f6486d3d9eb
cheese10yun <cheese10yun@gmail.com>
2022-05-28 22:49:19 +0900

    Modify jpa 주석

--- exposed-study/src/main/kotlin/com/example/exposedstudy/Foo.kt
+++ exposed-study/src/main/kotlin/com/example/exposedstudy/Foo.kt
@@ -1,27 +1,27 @@
-package com.example.exposedstudy
-
-import org.springframework.data.jpa.domain.support.AuditingEntityListener
-import org.springframework.data.jpa.repository.JpaRepository
-import java.io.Serializable
-import javax.persistence.*
-
-@Entity
-@Table(name = "foo")
-class Foo(
-    @Column(name = "title", nullable = false)
-    var title: String,
-) : AuditingEntityId()
-
-interface FooRepository : JpaRepository<Foo, Long>{
-    fun findByTitle(title: String): List<Foo>
-}
-
-@MappedSuperclass
-@EntityListeners(value = [AuditingEntityListener::class])
-abstract class AuditingEntityId : Serializable {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null
-        protected set
-}
+//package com.example.exposedstudy
+//
+//import org.springframework.data.jpa.domain.support.AuditingEntityListener
+//import org.springframework.data.jpa.repository.JpaRepository
+//import java.io.Serializable
+//import javax.persistence.*
+//
+//@Entity
+//@Table(name = "foo")
+//class Foo(
+//    @Column(name = "title", nullable = false)
+//    var title: String,
+//) : AuditingEntityId()
+//
+//interface FooRepository : JpaRepository<Foo, Long> {
+//    fun findByTitle(title: String): List<Foo>
+//}
+//
+//@MappedSuperclass
+//@EntityListeners(value = [AuditingEntityListener::class])
+//abstract class AuditingEntityId : Serializable {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    var id: Long? = null
+//        protected set
+//}
