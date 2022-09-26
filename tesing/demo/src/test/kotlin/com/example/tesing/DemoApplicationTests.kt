241085e2b822012a302893684ffffc5da2725eae
cheese10yun <cheese10yun@gmail.com>
2022-09-26 21:59:31 +0900

    init project

---
+++ tesing/demo/src/test/kotlin/com/example/tesing/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tesing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
