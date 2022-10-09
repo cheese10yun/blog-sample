8a5d667dba2e41ee11672053c74ca7e9042a5bfc
cheese10yun <cheese10yun@gmail.com>
2022-10-09 21:53:56 +0900

    정리

--- mockserver-netty/src/test/kotlin/com/example/demo/OrderServiceTest.kt
+++ mockserver-netty/src/test/kotlin/com/example/demo/OrderServiceTest.kt
@@ -4,15 +4,21 @@ import org.assertj.core.api.BDDAssertions.then
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.mockserver.integration.ClientAndServer
 import org.mockserver.model.HttpRequest
 import org.mockserver.model.HttpResponse
 import org.mockserver.model.Parameter
+import java.math.BigDecimal
 import java.time.LocalDate
 
-internal class OrderServiceTest(
-    private val orderService: OrderService,
-    private val orderQueryService: OrderQueryService
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class OrderServiceTest(
+//    private val orderService: OrderService,
+//    private val orderQueryService: OrderQueryService
 ) {
 
 
@@ -28,51 +34,76 @@ internal class OrderServiceTest(
         this.mockServer.stop()
     }
 
-    @Test
-    internal fun `환율 정보 기반으로 주문 금액 계산하여 주문 생성`() {
-        //given
-        val responseBody = """
-                    {
-                      "amount": "140000"
-                    }
-                """.trimIndent()
+//    @Test
+//    fun `환율 정보 기반으로 주문 금액 계산하여 주문 생성`() {
+//        //given
+//        val responseBody = """
+//                    {
+//                      "amount": "140000"
+//                    }
+//                """.trimIndent()
+//
+//
+//        mockServer.`when`(
+//            HttpRequest.request()
+//                .withMethod("GET")
+//                .withPath("/exchange-rate")
+//                .withPathParameters(
+//                    listOf(
+//                        Parameter.param("targetDate", "2022-02-02"),
+//                        Parameter.param("currencyForm", "USD"),
+//                        Parameter.param("currencyTo", "KRW")
+//                    )
+//                )
+//
+//        ).respond(
+//            HttpResponse.response()
+//                .withBody(responseBody)
+//                .withStatusCode(200)
+//        )
+//
+//        //when
+//        val orderNumber = orderService.order(1L, LocalDate.of(2022, 2, 2), 100.toBigDecimal())
+//
+//        //then
+//        val findOrder = orderQueryService.findOrderNumber(orderNumber)
+//        then(findOrder.amount).isEqualByComparingTo(140000.toBigDecimal())
+//    }
+
+//    @Test
+//    fun `Mock 객체를 기반으로 테스트`() {
+//        //given
+//
+//        //when
+//        val orderNumber = orderService.order(1L, LocalDate.of(2022, 2, 2), 100.toBigDecimal())
+//
+//        //then
+//        val findOrder = orderQueryService.findOrderNumber(orderNumber)
+//        then(findOrder.amount).isEqualByComparingTo(140000.toBigDecimal())
+//    }
 
 
-        mockServer.`when`(
-            HttpRequest.request()
-                .withMethod("GET")
-                .withPath("/exchange-rate")
-                .withPathParameters(
-                    listOf(
-                        Parameter.param("targetDate", "2022-02-02"),
-                        Parameter.param("currencyForm", "USD"),
-                        Parameter.param("currencyTo", "KRW")
-                    )
-                )
-
-        ).respond(
-            HttpResponse.response()
-                .withBody(responseBody)
-                .withStatusCode(200)
-        )
-
-        //when
-        val orderNumber = orderService.order(1L, LocalDate.of(2022, 2, 2), 100.toBigDecimal())
-
-        //then
-        val findOrder = orderQueryService.findOrderNumber(orderNumber)
-        then(findOrder.amount).isEqualByComparingTo(140000.toBigDecimal())
+    @ParameterizedTest
+    @MethodSource("providerExchangeRate")
+    fun `providerExchangeRate 테스트 `(exchangeRate: ExchangeRateProvider) {
+        println(exchangeRate)
     }
 
-    @Test
-    internal fun `Mock 객체를 기반으로 테스트`() {
-        //given
+    companion object {
+        @JvmStatic
+        fun providerExchangeRate() = listOf(
+            Arguments.of(ExchangeRateProvider(12000.toBigDecimal(), LocalDate.of(2022, 2, 2), "USD", "KRW")),
+            Arguments.of(ExchangeRateProvider(12000.toBigDecimal(), LocalDate.of(2022, 2, 3), "USD", "KRW")),
+            Arguments.of(ExchangeRateProvider(12000.toBigDecimal(), LocalDate.of(2022, 2, 4), "USD", "KRW"))
+        )
+    }
 
-        //when
-        val orderNumber = orderService.order(1L, LocalDate.of(2022, 2, 2), 100.toBigDecimal())
+    data class ExchangeRateProvider(
+        val amount: BigDecimal,
+        val targetDate: LocalDate,
+        val currencyForm: String,
+        val currencyTo: String
+    ) {
 
-        //then
-        val findOrder = orderQueryService.findOrderNumber(orderNumber)
-        then(findOrder.amount).isEqualByComparingTo(140000.toBigDecimal())
     }
 }
