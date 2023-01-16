803e75b8cec6f5f41ff3fdd0758be9f3c24198d5
cheese10yun <cheese10yun@gmail.com>
2023-01-16 21:01:34 +0900

    init

---
+++ lean-vue/lean-vue/vue-form/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data: function (){
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function () {
+
+      console.log(this.username, this.password);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
