3421f880f01e003372d57793df5c1856532d28b5
cheese10yun <cheese10yun@gmail.com>
2023-03-26 22:44:39 +0900

    setting

--- query-dsl/src/main/kotlin/com/example/querydsl/service/ApiService.kt
+++ query-dsl/src/main/kotlin/com/example/querydsl/service/ApiService.kt
@@ -1,16 +1,57 @@
 package com.example.querydsl.service
 
 import com.example.querydsl.domain.Team
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
+import java.math.BigDecimal
+import java.time.LocalDate
 
 @Service
 class ApiService(
-    private val restTemplate: RestTemplate
+    private val restTemplate: RestTemplate,
 ) {
 
     fun getTeam(name: String): List<Team> {
         return restTemplate.getForObject("/teams?name=$name", Array<Team>::class.java)!!.toList()
     }
 }
+
+@Service
+class OrderService(
+    private val exchangeRateObtainService: ExchangeRateObtainService,
+) {
+
+    /**
+     *
+     */
+    fun order(form: OrderForm) {
+        val exchangeRateResponse = exchangeRateObtainService.obtainExchangeRate(
+            date = LocalDate.now(),
+            form = "USD",
+            to = "KRW"
+        )
+    }
+
+}
+
+@Service
+class ExchangeRateObtainService(
+    private val restTemplate: RestTemplate,
+) {
+
+    fun obtainExchangeRate(
+        date: LocalDate,
+        form: String,
+        to: String,
+    ): ExchangeRateResponse {
+        return restTemplate.getForObject("/api/v1/exchange-rate?date=${date}&form=${form}&to=${to}", ExchangeRateResponse::class.java)!!
+    }
+}
+
+data class ExchangeRateResponse(
+    val exchangeRate: BigDecimal,
+)
+
+data class OrderForm(
+    val price: BigDecimal,
+)
---
+++ query-dsl/src/test/kotlin/com/example/querydsl/service/ApiServiceTest2.kt
@@ -0,0 +1,69 @@
+package com.example.querydsl.service
+
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.io.ClassPathResource
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.test.context.TestConstructor
+import org.springframework.test.web.client.MockRestServiceServer
+import org.springframework.test.web.client.match.MockRestRequestMatchers.method
+import org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo
+import org.springframework.test.web.client.response.MockRestResponseCreators.withStatus
+import org.springframework.web.client.RestTemplate
+import java.util.function.Consumer
+import kotlin.properties.Delegates.notNull
+
+
+@SpringBootTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+internal class ApiServiceTest2(
+    private val restTemplate: RestTemplate, // (1)
+    private val apiService: ApiService // (2)
+
+) {
+    private var server: MockRestServiceServer by notNull()
+
+    @BeforeEach
+    internal fun setUp() {
+        server = MockRestServiceServer.createServer(restTemplate) // (3)
+    }
+
+
+    @Test
+    internal fun `api test`() {
+        //given
+        val path = "/team-api-response.json"
+        server
+            .expect(
+                requestTo("http://localhost:8080/api/v1/exchange-rate?date=2023-03-20&form=USD&to=KRW")
+            )// (4)
+            .andExpect(method(HttpMethod.GET)) // (5)
+            .andRespond(
+                withStatus(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(ClassPathResource(path, javaClass))
+            )// (6)
+
+        //when
+        // (7)
+        val teams = apiService.getTeam("team")
+
+        //then
+        // (8)
+        for (team in teams) {
+            println(team)
+        }
+
+        // (9)
+        then(teams).hasSize(10)
+        then(teams).allSatisfy(
+            Consumer {
+                then(it.name).startsWith("team")
+            }
+        )
+    }
+}
