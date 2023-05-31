a84916c05c99b21f5612c24763eace6c17db7f9c
cheese10yun <cheese10yun@gmail.com>
2023-05-31 21:07:27 +0900

    이미지 주소 변경

--- intellij-test/intellij-test/src/main/resources/test.sql
+++ intellij-test/intellij-test/src/main/resources/test.sql
@@ -1,1 +1,10 @@
-insert INTO shop
+insert INTO shop(id, business_registration_number, name, address, address_detail, zip_code, rank)
+
+values
+    (1, '111111-1', 'test', 'test', 'test', test),
+    (1, '111111-1', 'test', 'test', 'test', test),
+    (1, '111111-1', 'test', 'test', 'test', test),
+    (1, '111111-1', 'test', 'test', 'test', test),
+    (1, '111111-1', 'test', 'test', 'test', test)
+
+;
