c109b01f4f680dd579f8a168745c3c6ad534ddf8
cheese10yun <cheese10yun@gmail.com>
2023-06-03 22:58:02 +0900

    init

--- intellij-test/intellij-test/src/main/kotlin/com/example/intellijtest/Shop.kt
+++ intellij-test/intellij-test/src/main/kotlin/com/example/intellijtest/Shop.kt
@@ -10,21 +10,32 @@ import jakarta.persistence.MappedSuperclass
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import org.springframework.data.jpa.repository.JpaRepository
 
-
 @Entity(name = "shop")
-data class Shop(
+class Shop(
     @Column(name = "business_registration_number", nullable = false)
     val brn: String,
     @Column(name = "name", nullable = false)
     val name: String,
+    @Column(name = "band", nullable = false)
+    val band: String,
+    @Column(name = "category", nullable = false)
+    val category: String,
+    @Column(name = "email", nullable = false)
+    val email: String,
+    @Column(name = "website", nullable = false)
+    val website: String,
+    @Column(name = "opening_hours", nullable = false)
+    val openingHours: String,
+    @Column(name = "seating_capacity", nullable = false)
+    val seatingCapacity: Int,
+    @Column(name = "rating", nullable = false)
+    val rating: Int,
     @Column(name = "address", nullable = false)
     val address: String,
     @Column(name = "address_detail", nullable = false)
     val addressDetail: String,
     @Column(name = "zip_code", nullable = false)
     val zipCode: String,
-    @Column(name = "rank", nullable = false)
-    val rank: String
 ) : EntityAuditing()
 
 
