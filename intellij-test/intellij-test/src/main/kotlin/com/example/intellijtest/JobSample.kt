fb0007d1bc1ee6c1610d28df71b8e2859746445c
cheese10yun <cheese10yun@gmail.com>
2023-06-04 03:25:28 +0900

    Add sample

---
+++ intellij-test/intellij-test/src/main/kotlin/com/example/intellijtest/JobSample.kt
@@ -0,0 +1,21 @@
+package com.example.intellijtest
+
+import jakarta.persistence.EntityManagerFactory
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.launch.support.RunIdIncrementer
+import org.springframework.batch.item.ItemProcessor
+import org.springframework.batch.item.ItemReader
+import org.springframework.batch.item.ItemWriter
+import org.springframework.context.annotation.Bean
+
+/**
+ * jbc
+ * jobcode
+ */
+class JobSample(
+) {
+
+}
