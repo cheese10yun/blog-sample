e00cf441be02a9ea88901e98dee5c0b592a71554
cheese10yun <cheese10yun@gmail.com>
2023-06-04 22:18:18 +0900

    Modify 주소 변경

--- intellij-test/intellij-test/src/main/resources/test.sql
+++ intellij-test/intellij-test/src/main/resources/test.sql
@@ -1,4 +1,4 @@
-insert INTO shop(id, business_registration_number, name, address, address_detail, zip_code, rank)
+insert INTO shop (id, business_registration_number, name, address, address_detail, zip_code, rank)
 
 values
     (1, '111111-1', 'test', 'test', 'test', test),
@@ -13,3 +13,22 @@ values
     (1, '111111-1', 'test', 'test', 'test', test),
     (1, '111111-1', 'test', 'test', 'test', test),
     (1, '111111-1', 'test', 'test', 'test', test)
+;
+
+
+insert INTO order (id, shopt_id, address, address_detail, zip_code, rank)
+
+values
+    (1, 1, 'address', 'address_detail', 1),
+    (1, 1, 'address', 'address_detail', 1),
+    (1, 1, 'address', 'address_detail', 1),
+    (1, 1, 'address', 'address_detail', 1),
+    (1, 1, 'address', 'address_detail', 1),
+    (1, 1, 'address', 'address_detail', 1),
+    (1, 1, 'address', 'address_detail', 1),
+    (1, 1, 'address', 'address_detail', 1),
+    (1, 1, 'address', 'address_detail', 1),
+    (1, 1, 'address', 'address_detail', 1),
+    (1, 1, 'address', 'address_detail', 1),
+    (1, 1, 'address', 'address_detail', 1)
+;
