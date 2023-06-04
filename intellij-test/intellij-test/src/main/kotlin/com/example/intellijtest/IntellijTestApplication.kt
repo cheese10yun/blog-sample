0cc4f2647dc27a35f17a9aa748871877afceb3f4
cheese10yun <cheese10yun@gmail.com>
2023-06-04 13:01:08 +0900

    Add sample

--- intellij-test/intellij-test/src/main/kotlin/com/example/intellijtest/IntellijTestApplication.kt
+++ intellij-test/intellij-test/src/main/kotlin/com/example/intellijtest/IntellijTestApplication.kt
@@ -1,11 +1,43 @@
 package com.example.intellijtest
 
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 class IntellijTestApplication
 
 fun main(args: Array<String>) {
     runApplication<IntellijTestApplication>(*args)
+}
+
+
+@Configuration
+class AppRunner(
+    private val shopRepository: ShopRepository,
+) : ApplicationRunner {
+    override fun run(args: ApplicationArguments) {
+
+        (1..5).map {
+            Shop(
+                brn = "brn$-{it}",
+                name = "name$-{it}",
+                band = "band$-{it}",
+                category = "category$-{it}",
+                email = "email$-{it}",
+                website = "website$-{it}",
+                openingHours = "openingHours$-{it}",
+                seatingCapacity = it,
+                rating = it,
+                address = "address$-{it}",
+                addressDetail = "addressDetail$-{it}",
+                zipCode = "zipCode$-{it}",
+            )
+        }.also {
+            shopRepository.saveAll(it)
+        }
+
+    }
 }
--- intellij-test/intellij-test/src/main/kotlin/com/example/intellijtest/ShopApi.kt
+++ intellij-test/intellij-test/src/main/kotlin/com/example/intellijtest/ShopApi.kt
@@ -3,11 +3,13 @@ package com.example.intellijtest
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/shop")
 class ShopApi(
     private val shopRepository: ShopRepository,
 ) {
@@ -15,11 +17,33 @@ class ShopApi(
     @GetMapping
     fun getShop(
         @PageableDefault pageable: Pageable,
+    ): List<Shop> {
+
+        return shopRepository.findAll()
+    }
+
+    @PostMapping
+    fun save(
+        @RequestBody dto: ShopRegistrationRequest,
     ) {
 
-        shopRepository.findAll()
     }
 }
+
+class ShopRegistrationRequest(
+    val brn: String,
+    val name: String,
+    val band: String,
+    val category: String,
+    val email: String,
+    val website: String,
+    val openingHours: String,
+    val seatingCapacity: Int,
+    val rating: Int,
+    val address: String,
+    val addressDetail: String,
+    val zipCode: String,
+)
 
 class ShopResponse(shop: Shop) {
 
