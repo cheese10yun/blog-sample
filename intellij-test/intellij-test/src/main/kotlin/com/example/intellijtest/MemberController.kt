baffed1f4c702869d0b8c11e45a862fc60ba1d57
cheese10yun <cheese10yun@gmail.com>
2023-06-10 16:21:44 +0900

    Add query dsl setting

---
+++ intellij-test/intellij-test/src/main/kotlin/com/example/intellijtest/MemberController.kt
@@ -0,0 +1,31 @@
+package com.example.intellijtest
+
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/members")
+class MemberController {
+
+    @PostMapping
+    fun register(
+        @RequestBody dto: MemberRegistrationRequest
+    ) {
+        // ...
+    }
+}
+
+data class MemberRegistrationRequest(
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val phoneNumber: String,
+    val address: String,
+    val age: Int,
+    val gender: String,
+    val occupation: String,
+)
