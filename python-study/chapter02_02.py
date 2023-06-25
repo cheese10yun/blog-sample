0a40487c2944903e92d37ffd3a3ffc7122aa6c55
cheese10yun <cheese10yun@gmail.com>
2023-06-25 22:24:08 +0900

    init

---
+++ python-study/chapter02_02.py
@@ -0,0 +1,32 @@
+
+n = 700
+
+print(n)
+print(type(n))
+
+
+x = y = z = 700
+
+print(x, y, z)
+
+
+var = 75
+
+var = 'Change Value'
+
+
+
+print(var)
+print(type(var))
+
+
+m = 800
+z = 800
+n = 655
+
+print(id(m))
+print(id(z))
+print(id(n))
+
+print(id(m) == id(z))
+print()
--- python-study/test.py
+++ python-study/test.py
@@ -1,16 +1,18 @@
-# import unittest
-# import platform
-#
-#
-# class MyTestCase(unittest.TestCase):
-#      def test_something(self):
-#         print(platform.processor())
-#         # self.assertEqual(True, False)  # add assertion here
-#
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
+import unittest
+import platform
 
 
-print("hello python!")
+class MyTestCase(unittest.TestCase):
+     def test_something(self):
+        print(platform.version())
+        print(platform.python_version())
+        print(platform.processor())
+        # self.assertEqual(True, False)  # add assertion here
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+# print("hello python!")
