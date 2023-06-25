b9adc058691396c5e5f42b8f62031d5be743d5aa
cheese10yun <cheese10yun@gmail.com>
2023-06-25 22:01:53 +0900

    init

---
+++ python-study/chapter02_01.py
@@ -0,0 +1,27 @@
+# # Print 사용법
+#
+#
+# print('Python Start!')
+# print("Python Start!")
+# print('''Python Start!''')
+# print("""Python Start!""")
+#
+# print()
+# print()
+# print()
+#
+#
+# print('p', 'y', 't', 'h', 'o', 'n', sep='')
+#
+# print("welcome to ", end='')
+# print("IT news", end='')
+#
+# import sys
+#
+# # print('Lean Python', file='')
+#
+#
+print('%s %s' % ('one', 'two'))
+print('{} {}'.format('one', 'two'))
+print('{1} {0}'.format('one', 'two'))
+print('sout')
--- python-study/test.py
+++ python-study/test.py
@@ -1,13 +1,16 @@
-import unittest
-import platform
+# import unittest
+# import platform
+#
+#
+# class MyTestCase(unittest.TestCase):
+#      def test_something(self):
+#         print(platform.processor())
+#         # self.assertEqual(True, False)  # add assertion here
+#
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
 
 
-class MyTestCase(unittest.TestCase):
-     def test_something(self):
-        print(platform.processor())
-        # self.assertEqual(True, False)  # add assertion here
-
-
-
-if __name__ == '__main__':
-    unittest.main()
+print("hello python!")
