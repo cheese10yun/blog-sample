7f914c9247619772ca78ea6988b9ad89c3e225a1
cheese10yun <cheese10yun@gmail.com>
2023-07-03 00:35:00 +0900

    init

--- tobyspring/tobyspring/src/main/java/com/example/tobyspring/TobyspringApplication.java
+++ tobyspring/tobyspring/src/main/java/com/example/tobyspring/TobyspringApplication.java
@@ -2,12 +2,42 @@ package com.example.tobyspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@SpringBootApplication
 public class TobyspringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TobyspringApplication.class, args);
-    }
+//        SpringApplication.run(TobyspringApplication.class, args);
 
+        final TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        final WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+                    final String name = req.getParameter("name");
+
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hello Servlet " + name);
+
+                }
+            }).addMapping("/hello");
+        });
+
+        webServer.start();
+    }
 }
