b8e12e217fdc0841fb8f02ce21294841167f88b2
cheese10yun <cheese10yun@gmail.com>
2023-07-16 01:16:24 +0900

    init

--- tobyspring/tobyspring/src/main/java/com/example/tobyspring/MyApplication.java
+++ tobyspring/tobyspring/src/main/java/com/example/tobyspring/MyApplication.java
@@ -26,36 +26,7 @@ public class MyApplication {
                 webServer.start();
             }
         };
-//        applicationContext.registerBean(HelloController.class);
-//        applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.register(applicationClass);
         applicationContext.refresh();
-
-
-//        final WebServer webServer = serverFactory.getWebServer(servletContext -> {
-////            final HelloController helloController = new HelloController();
-//            servletContext.addServlet("frontcontroller", new HttpServlet() {
-//                @Override
-//                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-//                        final String name = req.getParameter("name");
-//
-//                        final HelloController helloController = applicationContext.getBean(HelloController.class);
-//
-//                        final String ret = helloController.hello(name);
-//                        resp.setStatus(HttpStatus.OK.value());
-//                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-//                        resp.getWriter().println(ret);
-//                    } else if (req.getRequestURI().equals("/user")) {
-//                        //
-//                    } else {
-//                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-//                    }
-//
-//
-//                }
-//            }).addMapping("/hello");
-//        });
     }
-
 }
