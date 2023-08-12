cc1d1fd5de2beda1225ded99937596fc0dbee6da
cheese10yun <cheese10yun@gmail.com>
2023-08-13 03:26:24 +0900

    init

--- kotlin-coroutine/src/test/kotlin/com/example/kotlincoroutine/ActorCoroutine.kt
+++ kotlin-coroutine/src/test/kotlin/com/example/kotlincoroutine/ActorCoroutine.kt
@@ -8,6 +8,8 @@ import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.newFixedThreadPoolContext
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.junit.jupiter.api.Test
 
 
@@ -142,6 +144,57 @@ class ActorCoroutine5 {
         for (i in 1..10){
             actor.send("a")
         }
+    }
+}
+
+class ActorCoroutine6 {
+
+    val mutex = Mutex()
+    var counter = 0
+
+    @Test
+    fun `코루틴 단일 스레드로 한정`(): Unit = runBlocking {
+        val context = newSingleThreadContext("counter")
+        val workerA = asyncIncrement(2000)
+        val workerB = asyncIncrement(200)
+
+        workerA.await()
+        workerB.await()
+
+        print("Counter: [${counter}]")
+    }
+
+    fun asyncIncrement(by: Int) = GlobalScope.async() {
+        for (i in 0 until by) {
+            mutex.withLock {
+                counter++
+            }
+        }
+    }
+}
+
+class ActorCoroutine7 {
+
+
+    @Test
+    fun `코루틴 단일 스레드로 한정`(): Unit = runBlocking {
+        val mutex = Mutex()
+        mutex.lock() // 잠금이 이미 설정된 경우 일시 중단된다.
+        print("I am now an atomic block")
+        mutex.unlock() // 이것은 중단되지 않는다.
+    }
+
+    suspend fun asd() {
+        val mutex = Mutex()
+        mutex.lock()
+        mutex.isLocked // true
+        mutex.unlock()
+    }
+
+    suspend fun asd2() {
+        val mutex = Mutex()
+        mutex.tryLock() // true
+        mutex.unlock()
     }
 }
 
