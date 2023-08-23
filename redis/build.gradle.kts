1ed0b3fdfa02790395c6a887a0c7aaad5b79064d
cheese10yun <cheese10yun@gmail.com>
2023-08-24 01:49:08 +0900

    Modify version

--- redis/build.gradle.kts
+++ redis/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.2"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.20"
-    kotlin("plugin.spring") version "1.5.20"
+    id("org.springframework.boot") version "2.7.14"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
 }
 
 group = "com.example"
