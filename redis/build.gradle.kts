1077612e74f53cf6cf1a8ca2821d163c6af5b34c
cheese10yun <cheese10yun@gmail.com>
2023-08-26 00:22:25 +0900

    Modify version

--- redis/build.gradle.kts
+++ redis/build.gradle.kts
@@ -20,6 +20,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+
+    implementation("org.springframework.session:spring-session-data-redis")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2")
---
+++ redis/src/main/kotlin/com/example/redis/HelloController.kt
@@ -0,0 +1,15 @@
+package com.example.redis
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/hello")
+class HelloController {
+
+    @GetMapping
+    fun hello(): String {
+        return "Hello"
+    }
+}
