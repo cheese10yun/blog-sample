0c8f8a38b2e679bfbc14a29bf3e4f0cc825a2df0
cheese10yun <cheese10yun@gmail.com>
2023-09-01 02:26:50 +0900

    init

--- query-dsl/settings.gradle.kts
+++ query-dsl/settings.gradle.kts
@@ -9,8 +9,8 @@ pluginManagement {
     val pluginVersions = mapOf(
         "org.jetbrains.kotlin" to "1.6.21",
         "org.jetbrains.kotlin.plugin" to "1.6.21",
-        "org.springframework" to "2.7.0",
-        "io.spring" to "1.0.10.RELEASE"
+        "org.springframework" to "2.7.4",
+        "io.spring" to "1.0.14.RELEASE"
     )
 
     resolutionStrategy {
