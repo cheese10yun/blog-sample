02e838ff872734bde7af3d9169bd82be23e4f2cc
cheese10yun <cheese10yun@gmail.com>
2023-09-01 02:47:21 +0900

    주소 변경

--- exposed-study/build.gradle.kts
+++ exposed-study/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 //    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8") //
