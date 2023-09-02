480bcb8c3aa0586f6adb120fdba63dff3d02ceeb
cheese10yun <cheese10yun@gmail.com>
2023-09-03 01:43:46 +0900

    init

--- query-dsl/src/main/kotlin/com/example/querydsl/repository/order/OrderRepository.kt
+++ query-dsl/src/main/kotlin/com/example/querydsl/repository/order/OrderRepository.kt
@@ -2,7 +2,18 @@ package com.example.querydsl.repository.order
 
 
 import com.example.querydsl.domain.EntityAuditing
+import com.example.querydsl.repository.order.QOrder.order
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
+import org.springframework.data.web.PageableDefault
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Table
@@ -22,4 +33,36 @@ class Order(
 
 ) : EntityAuditing()
 
-interface OrderRepository : JpaRepository<Order, Long>
+interface OrderRepository : JpaRepository<Order, Long>, OrderCustomRepository
+
+interface OrderCustomRepository {
+    fun find(pageable: Pageable): Page<Order>
+}
+
+class OrderCustomRepositoryImpl : QuerydslRepositorySupport(Order::class.java), OrderCustomRepository {
+
+    override fun find(
+        pageable: Pageable
+    ) =  runBlocking {
+        val contentQuery = from(order).select(order).where(order.userId.isNotNull)
+        val countQuery = from(order).select(order.count()).where(order.userId.isNotNull)
+        val content = async { contentQuery.fetch() }
+        val count = async { countQuery.fetchFirst() }
+
+        PageImpl(content.await(), pageable, count.await())
+    }
+}
+
+@RestController
+@RequestMapping("/api/orders")
+class OrderApi(
+    private val orderRepository: OrderRepository
+) {
+
+    @GetMapping
+    fun getOrder(
+        @PageableDefault pageable: Pageable
+    ): Page<Order> {
+        return orderRepository.find(pageable)
+    }
+}
