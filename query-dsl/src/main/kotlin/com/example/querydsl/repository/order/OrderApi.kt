4404ec56365c1056ff9ee903cf2fba998f7ab679
cheese10yun <cheese10yun@gmail.com>
2023-09-03 20:56:07 +0900

    init

---
+++ query-dsl/src/main/kotlin/com/example/querydsl/repository/order/OrderApi.kt
@@ -0,0 +1,39 @@
+package com.example.querydsl.repository.order
+
+import com.example.querydsl.logger
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
+import org.springframework.data.web.PageableDefault
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/orders")
+class OrderApi(
+    private val orderRepository: OrderRepository
+) {
+    private val log by logger()
+
+    @GetMapping
+    fun getOrder(
+        @PageableDefault pageable: Pageable,
+        @RequestParam(name = "address") address: String
+
+    ): Page<Order> {
+        log.info("thread api : ${Thread.currentThread()}")
+//        return orderRepository.findPaging1(pageable)
+        return orderRepository.findPaging2(pageable, address)
+    }
+
+    @GetMapping("/slice")
+    fun getOrderSlice(
+        @PageableDefault pageable: Pageable,
+        @RequestParam(name = "address") address: String
+    ): Slice<Order> {
+        log.info("thread api : ${Thread.currentThread()}")
+        return orderRepository.findSlice(pageable, address)
+    }
+}
--- query-dsl/src/main/kotlin/com/example/querydsl/repository/order/OrderRepository.kt
+++ query-dsl/src/main/kotlin/com/example/querydsl/repository/order/OrderRepository.kt
@@ -13,11 +13,6 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Slice
 import org.springframework.data.domain.SliceImpl
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.web.PageableDefault
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Table
@@ -99,32 +94,4 @@ class OrderCustomRepositoryImpl : Querydsl4RepositorySupport(Order::class.java),
 //    private fun SliceImpl(fetch: List<Order>, pageable: Pageable): Slice<Order> {
 //        TODO("Not yet implemented")
 //    }
-}
-
-@RestController
-@RequestMapping("/api/orders")
-class OrderApi(
-    private val orderRepository: OrderRepository
-) {
-    private val log by logger()
-
-    @GetMapping
-    fun getOrder(
-        @PageableDefault pageable: Pageable,
-        @RequestParam(name = "address") address: String
-
-    ): Page<Order> {
-        log.info("thread api : ${Thread.currentThread()}")
-//        return orderRepository.findPaging1(pageable)
-        return orderRepository.findPaging2(pageable, address)
-    }
-
-    @GetMapping("/slice")
-    fun getOrderSlice(
-        @PageableDefault pageable: Pageable,
-        @RequestParam(name = "address") address: String
-    ): Slice<Order> {
-        log.info("thread api : ${Thread.currentThread()}")
-        return orderRepository.findSlice(pageable, address)
-    }
 }
