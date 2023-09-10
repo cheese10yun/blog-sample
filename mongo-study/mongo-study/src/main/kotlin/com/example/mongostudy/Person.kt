0282e445172dd0894882a17016d0229d815e95f5
cheese10yun <cheese10yun@gmail.com>
2023-09-10 16:21:38 +0900

    init

--- mongo-study/mongo-study/src/main/kotlin/com/example/mongostudy/Person.kt
+++ mongo-study/mongo-study/src/main/kotlin/com/example/mongostudy/Person.kt
@@ -1,8 +1,13 @@
 package com.example.mongostudy
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.isEqualTo
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Service
 
 @Document(collection = "persons")
 data class Person(
@@ -13,3 +18,15 @@ data class Person(
 )
 
 interface PersonRepository : MongoRepository<Person, String>
+
+@Service
+class PersonQueryService(
+    private val personRepository: PersonRepository,
+    private val mongoTemplate: MongoTemplate
+) {
+
+    fun findBy(firstName: String): List<Person> {
+        val query = Query.query(Criteria.where("firstName").isEqualTo(firstName))
+        return mongoTemplate.find(query, Person::class.java)
+    }
+}
--- mongo-study/mongo-study/src/main/kotlin/com/example/mongostudy/PersonController.kt
+++ mongo-study/mongo-study/src/main/kotlin/com/example/mongostudy/PersonController.kt
@@ -5,17 +5,26 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/persons")
 class PersonController(
-    private val personRepository: PersonRepository
+    private val personRepository: PersonRepository,
+    private val personQueryService: PersonQueryService
 ) {
 
     @GetMapping
     fun getAllPersons(): List<Person> {
         return personRepository.findAll()
+    }
+
+    @GetMapping("/query")
+    fun getQuery(
+        @RequestParam("firstName") firstName: String
+    ): List<Person> {
+        return personQueryService.findBy(firstName)
     }
 
     @PostMapping("/")
