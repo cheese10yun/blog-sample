8febd4793c867639652eddb1a983dc060bb73117
cheese10yun <cheese10yun@gmail.com>
2023-09-23 21:54:58 +0900

    정리

--- restdocs-sample/src/main/kotlin/com/example/restdocssample/member/Member.kt
+++ restdocs-sample/src/main/kotlin/com/example/restdocssample/member/Member.kt
@@ -9,7 +9,6 @@ import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -22,6 +21,9 @@ import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.Table
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
 
 @RestController
@@ -52,8 +54,14 @@ class MemberApi(
 
 
 class MemberResponse(member: Member) {
+    @field:NotNull
     val id = member.id!!
+    @field:Email
+    @field:NotEmpty
+    @field:NotNull
     val email = member.email
+    @field:NotEmpty
+    @field:NotNull
     val name = member.name
 }
 
