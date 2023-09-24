b5368d6dffc22dc39cafb8f9ad4e6d9e1821b61b
cheese10yun <cheese10yun@gmail.com>
2023-09-24 18:20:57 +0900

    정리

--- restdocs-sample/swagger-node-server/server.js
+++ restdocs-sample/swagger-node-server/server.js
@@ -12,6 +12,25 @@ const files = fs.readdirSync(openapiDir);
 // 'openapi3'로 시작하는 yaml 파일만 필터링
 const openapiFiles = files.filter(file => file.startsWith('openapi3') && file.endsWith('.yaml'));
 
+openapiFiles.forEach(file => {
+    const filePath = path.join(openapiDir, file);
+    const document = YAML.load(filePath);
+
+    document.components = document.components || {};
+
+    // securitySchemes에 ApiKeyAuth가 없는 경우에만 추가
+    if (!document.components.securitySchemes || !document.components.securitySchemes.ApiKeyAuth) {
+        document.components.securitySchemes = {
+            ApiKeyAuth: {
+                type: 'apiKey',
+                in: 'header',
+                name: 'x-request-service'
+            }
+        };
+    }
+    fs.writeFileSync(filePath, YAML.stringify(document, 8, 2));
+});
+
 // 파일 이름을 가공하여 options에 추가
 const swaggerOptions = {
     urls: openapiFiles.map(file => {
