58215f997b69f90b4069e08033b728c506032ec9
cheese10yun <cheese10yun@gmail.com>
2023-09-26 22:09:01 +0900

    plugins 적용

--- restdocs-sample/build.gradle.kts
+++ restdocs-sample/build.gradle.kts
@@ -1,5 +1,9 @@
 import groovy.lang.Closure
 import io.swagger.v3.oas.models.servers.Server
+import kotlin.collections.Map
+import kotlin.collections.get
+import kotlin.collections.listOf
+import kotlin.collections.set
 import org.yaml.snakeyaml.Yaml
 import java.io.InputStream
 
@@ -13,6 +17,8 @@ plugins {
     kotlin("plugin.jpa") version "1.8.20"
 }
 
+apply(plugin = "openapi-upload")
+
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
@@ -20,6 +26,8 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 repositories {
     mavenCentral()
 }
+
+val openApiFileName = "openapi-${getSpringAppName().orEmpty()}.yaml"
 
 lateinit var asciidoctorExt: Configuration
 val snippetsDir by extra { file("build/generated-snippets") }
@@ -47,8 +55,18 @@ dependencies {
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("com.epages:restdocs-api-spec-mockmvc:0.16.4")
 
+    implementation("io.ktor:ktor-client-core:2.1.1") // Ktor core
+    implementation("io.ktor:ktor-client-cio:2.1.1") // CIO 엔진
+    implementation("io.ktor:ktor-client-logging:2.1.1")
+    implementation("io.ktor:ktor-client-content-negotiation:2.1.1")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.1")
+
+    testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter-kotlin:0.6.8")
+
     runtimeOnly("com.h2database:h2")
     asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
+
+    implementation("org.apache.httpcomponents:httpclient:4.5.13")
 }
 
 openapi3 {
@@ -73,7 +91,7 @@ openapi3 {
     tagDescriptionsPropertiesFile = "src/test/resources/tags-descriptions.yaml"
     version = "0.1.0"
     format = "yaml"
-    outputFileNamePrefix = "openapi-${getSpringAppName().orEmpty()}"
+    outputFileNamePrefix = "openapi3-${getSpringAppName().orEmpty()}"
 }
 
 postman {
