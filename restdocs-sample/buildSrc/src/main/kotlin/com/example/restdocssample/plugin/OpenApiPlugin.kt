61c2e91986a32925c03a4207b15189386a964560
cheese10yun <cheese10yun@gmail.com>
2023-10-14 21:19:20 +0900

    plugins 적용

--- restdocs-sample/buildSrc/src/main/kotlin/com/example/restdocssample/plugin/OpenApiPlugin.kt
+++ restdocs-sample/buildSrc/src/main/kotlin/com/example/restdocssample/plugin/OpenApiPlugin.kt
@@ -1,27 +1,20 @@
 package com.example.restdocssample.plugin
 
+import com.epages.restdocs.apispec.gradle.ApiSpecTask
+import com.epages.restdocs.apispec.gradle.OpenApi3Extension
+import com.epages.restdocs.apispec.gradle.OpenApi3Task
+import com.epages.restdocs.apispec.gradle.OpenApiBaseExtension
+import com.epages.restdocs.apispec.gradle.OpenApiBaseTask
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class OpenApiPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.afterEvaluate {
-            val generateOpenApi3Task = tasks.findByName("openapi3")
-            generateOpenApi3Task?.doLast {
-                // 여기에 추가적인 동작을 넣습니다.
-                logger.info("This will run after openapi3 task!")
-            }
+//            val generateOpenApi3Task = tasks.findByName("openapi3") as OpenApiBaseExtension
+
+
         }
     }
-}
-
-
-open class OpenApiExtension {
-    var title: String = "Member API"
-    var description: String = "My API description"
-    var tagDescriptionsPropertiesFile: String = "src/test/resources/tags-descriptions.yaml"
-    var version: String = "0.1.0"
-    var format: String = "yaml"
-    // getSpringAppName() 함수의 구현은 별도로 처리되어야 합니다.
-    val outputFileNamePrefix: String = "openapi3-"
 }
