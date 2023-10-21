1883829cf9b45284ac085b3da024bc96421b6243
cheese10yun <cheese10yun@gmail.com>
2023-10-21 12:51:30 +0900

    Add mongo transaction manager

--- mongo-study/mongo-study/src/main/kotlin/com/example/mongostudy/config/MongoConfig.kt
+++ mongo-study/mongo-study/src/main/kotlin/com/example/mongostudy/config/MongoConfig.kt
@@ -1,8 +1,19 @@
 package com.example.mongostudy.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.MongoDatabaseFactory
+import org.springframework.data.mongodb.MongoTransactionManager
 import org.springframework.data.mongodb.config.EnableMongoAuditing
+
+const val MONGO_TRANSACTION_MANAGER = "mongoTransactionManager"
 
 @Configuration
 @EnableMongoAuditing
-class MongoConfig
+class MongoConfig {
+
+    @Bean(MONGO_TRANSACTION_MANAGER)
+    fun mongoTransactionManager(factory: MongoDatabaseFactory) =
+        MongoTransactionManager(factory)
+
+}
