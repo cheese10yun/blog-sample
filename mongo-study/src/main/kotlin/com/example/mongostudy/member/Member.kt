d76b86dc477ad252061ef93aa5a34bece7dc3623
yun.cheese <yun.cheese@kakaocorp.com>
2023-10-28 16:57:00 +0900

    init

--- mongo-study/src/main/kotlin/com/example/mongostudy/member/Member.kt
+++ mongo-study/src/main/kotlin/com/example/mongostudy/member/Member.kt
@@ -6,6 +6,7 @@ import com.example.mongostudy.mongo.fieldName
 import org.bson.types.ObjectId
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.index.CompoundIndex
 import org.springframework.data.mongodb.core.index.CompoundIndexes
@@ -74,6 +75,12 @@ interface MemberCustomRepository {
         dateJoinedTo: LocalDateTime?,
         memberStatus: MemberStatus?
     ): Page<Member>
+
+    fun findSlice(
+        pageable: Pageable,
+        name: String?,
+        email: String?
+    ): Slice<Member>
 }
 
 class MemberCustomRepositoryImpl(mongoTemplate: MongoTemplate) : MemberCustomRepository, MongoCustomRepositorySupport<Member>(
@@ -120,15 +127,28 @@ class MemberCustomRepositoryImpl(mongoTemplate: MongoTemplate) : MemberCustomRep
             query.addCriteria(criteria)
         }
 
-
         return applyPagination(
             pageable = pageable,
-            contentQuery = { query ->
-                mongoTemplate.find(queryBuilder(query), documentClass)
-            },
-            countQuery = { query ->
-                mongoTemplate.count(queryBuilder(query), documentClass)
+            contentQuery = { mongoTemplate.find(queryBuilder(it), documentClass) },
+            countQuery = { mongoTemplate.count(queryBuilder(it), documentClass) }
+        )
+    }
+
+    override fun findSlice(
+        pageable: Pageable,
+        name: String?,
+        email: String?
+    ): Slice<Member> {
+        val queryBuilder: (Query) -> Query = { query ->
+            val criteria = Criteria().apply {
+                name?.let { and(Member::name.fieldName()).regex(".*$it.*", "i") }
+                email?.let { and(Member::email.fieldName()).regex(".*$it.*", "i") }
             }
+            query.addCriteria(criteria)
+        }
+        return applySlicePagination(
+            pageable = pageable,
+            contentQuery = { mongoTemplate.find(queryBuilder(it), documentClass) }
         )
     }
 }
