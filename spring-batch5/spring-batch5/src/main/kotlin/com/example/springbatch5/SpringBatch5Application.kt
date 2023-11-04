21d324e595afc44b17192d57368abf414a7782ed
Yun <cheese10yun@gmail.com>
2023-11-05 00:36:10 +0900

    init

---
+++ spring-batch5/spring-batch5/src/main/kotlin/com/example/springbatch5/SpringBatch5Application.kt
@@ -0,0 +1,11 @@
+package com.example.springbatch5
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBatch5Application
+
+fun main(args: Array<String>) {
+    runApplication<SpringBatch5Application>(*args)
+}
---
+++ spring-batch5/spring-batch5/src/test/kotlin/com/example/springbatch5/SpringBatch5ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbatch5
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBatch5ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
