36cf7b548b6463af25f4e090f332d7a8ea7b9065
Yun <cheese10yun@gmail.com>
2024-08-12 22:15:22 +0900

    수정

--- spring-camp-test/domain/src/test/kotlin/com/spring/camp/domain/OrderFixtureTest.kt
+++ spring-camp-test/domain/src/test/kotlin/com/spring/camp/domain/OrderFixtureTest.kt
@@ -1,41 +1,39 @@
 package com.spring.camp.domain
 
 import java.time.LocalDate
-import org.assertj.core.api.BDDAssertions.then
 import org.junit.jupiter.api.Test
 
 
 class OrderFixtureTest(
-    private val orderRepository: OrderRepository
+    private val orderRepository: OrderRepository,
+//    private val xxxService: XxxService
 ) {
 
-//    @Test
-//    fun `조회 테스트`() {
-//        // given
-//        val customerName = "Willie Carlson"
-//        val status = "DICTAS"
-//        val orderDate = LocalDate.of(2024, 1, 2)
-//        Order(
-//            customerName = customerName,
-//            orderDate = orderDate,
-//            totalAmount = 8.9,
-//            status = status,
-//            shippingAddress = "sapien",
-//            billingAddress = "ridens",
-//            paymentMethod = "libero",
-//            shippingCost = 10.11,
-//            taxAmount = 12.13,
-//            discount = 14.15,
-//            notes = null
-//        )
+    @Test
+    fun `쿠폰 계산 테스트 코드`() {
+        // given
+        val customerName = "Willie Carlson"
+        val status = "DICTAS"
+        val orderDate = LocalDate.of(2024, 1, 2)
+        val order = DomainFixTure.order(
+            customerName = customerName,
+            orderDate = orderDate,
+            totalAmount = 8.9,
+            status = status
+        )
+        val coupon = DomainFixTure.coupon(
+            code = "COUPON_CODE",
+            order = order
+        )
+//        save(order)
+//        save(coupon)
 //
 //        // when
-//        val orders = orderRepository.findByCustomerNameAndStatusAndOrderDate(
-//            customerName = customerName,
-//            status = status,
-//            orderDate = orderDate
+//        xxxService.xxxx(
+//            order = order,
+//            coupon = coupon
 //        )
 //        // then
 //        then(...)
-//    }
+    }
 }
--- spring-camp-test/domain/src/testFixtures/kotlin/com/spring/camp/domain/DomainFixTure.kt
+++ spring-camp-test/domain/src/testFixtures/kotlin/com/spring/camp/domain/DomainFixTure.kt
@@ -3,24 +3,46 @@ package com.spring.camp.domain
 import java.time.LocalDate
 
 object DomainFixTure {
-
     fun order(
-        s: String = "customerName",
-        localDate: LocalDate? = LocalDate.now()
-    ) {
-        Order(
-            customerName = s,
-            orderDate = localDate,
-            totalAmount = 8.9,
-            status = "READY",
-            shippingAddress = "sapien",
-            billingAddress = "ridens",
-            paymentMethod = "libero",
-            shippingCost = 10.11,
-            taxAmount = 12.13,
-            discount = 14.15,
-            notes = null
+        customerName: String = "홍길동",
+        orderDate: LocalDate = LocalDate.of(2024, 4, 4),
+        totalAmount: Double = 8.9,
+        status: String = "READY",
+        shippingAddress: String = "서울 특별시 xxx 구, xxx로 123",
+        billingAddress: String = "서울 특별시 xxx 구, xxx로 123",
+        paymentMethod: String = "CARD",
+        shippingCost: Double = 10.11,
+        taxAmount: Double = 12.13,
+        discount: Double = 14.15,
+        notes: String? = null
+    ): Order {
+        return Order(
+            customerName = customerName,
+            orderDate = orderDate,
+            totalAmount = totalAmount,
+            status = status,
+            shippingAddress = shippingAddress,
+            billingAddress = billingAddress,
+            paymentMethod = paymentMethod,
+            shippingCost = shippingCost,
+            taxAmount = taxAmount,
+            discount = discount,
+            notes = notes,
         )
     }
 
+    fun coupon(
+        code: String = "COUPON_CODE",
+        order: Order
+    ): Coupon {
+        return Coupon(
+            code = code,
+            id = null
+        )
+    }
 }
+
+data class Coupon(
+    val id: Long? = null,
+    val code: String
+)
