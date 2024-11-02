3db079819831a987ec177cf8bf8952a6fe824c48
Yun <cheese10yun@gmail.com>
2024-11-02 22:57:41 +0900

    init

---
+++ KafkaProj-01/KafkaProj-01/producers/src/main/java/com/example/kafka/SimpleProducerASync.java
@@ -0,0 +1,45 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
+
+public class SimpleProducerASync {
+
+    private static final Logger logger = Logger.getLogger(SimpleProducerASync.class.getName());
+
+    public static void main(String[] args) {
+        final String topicName = "simple-topic";
+        final Properties props = new Properties();
+
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "121.168.58.79:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        final KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+        final ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, "hello world 2");
+
+        kafkaProducer.send(producerRecord, (metadata, exception) -> {
+            if (exception == null) {
+                logger.info("\n##### record metadata received #### \n" +
+                        "partition: " + metadata.partition() + "\n" +
+                        "offset: " + metadata.offset() + "\n" +
+                        "timestamp: " + metadata.timestamp() + "\n"
+                );
+            } else {
+                logger.info("exception error from broker : " + exception.getMessage());
+            }
+        });
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        kafkaProducer.close();
+    }
+}
