51cb4172115c0e46afc7f51ccefe9c56e50c0b41
Yun <cheese10yun@gmail.com>
2026-01-17 22:56:15 +0900

    초안 작성

---
+++ spring-batch5/src/main/java/com/example/springbatch5/SpringBatch5Application.java
@@ -0,0 +1,13 @@
+package com.example.springbatch5;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch5Application.class, args);
+	}
+}
