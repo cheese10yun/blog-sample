56b48186c3b1beaa52c00167f6d5d1a21bd88ba8
Yun <cheese10yun@gmail.com>
2026-01-20 02:05:50 +0900

    정리

--- boot3-mongo/build.gradle.kts
+++ boot3-mongo/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    kotlin("plugin.jpa") version "1.9.25"
+//    kotlin("plugin.jpa") version "1.9.25"
     kotlin("kapt") version "1.9.25"
     id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
@@ -35,7 +35,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -54,17 +54,17 @@ dependencies {
     implementation("co.elastic.apm:apm-agent-api:1.52.2")
     implementation("co.elastic.apm:apm-opentracing:1.52.2")
 //    implementation("io.opentracing.contrib:opentracing-spring-cloud-mongo-starter:0.5.9")
-    implementation("io.opentracing.contrib:opentracing-spring-cloud-starter:0.5.9")
-    implementation("io.opentracing.contrib:opentracing-mongo-driver:0.1.5")
+//    implementation("io.opentracing.contrib:opentracing-spring-cloud-starter:0.5.9")
+//    implementation("io.opentracing.contrib:opentracing-mongo-driver:0.1.5")
 
 
 
-    runtimeOnly("com.mysql:mysql-connector-j")
+//    runtimeOnly("com.mysql:mysql-connector-j")
 
-    annotationProcessor(group = "com.querydsl", name = "querydsl-apt", classifier = "jpa", version = "5.1.0")
+//    annotationProcessor(group = "com.querydsl", name = "querydsl-apt", classifier = "jpa", version = "5.1.0")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-    kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
+//    kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
 }
 
 kotlin {
